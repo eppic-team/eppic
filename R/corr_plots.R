d2bcd3d57a6ec46dbab9cd2c1689f0dd31268ac2
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2010-10-12 12:06:36 +0000

    Some new plots

--- R/corr_plots.R
+++ R/corr_plots.R
@@ -5,6 +5,7 @@ dnw<-read.table(nwfile)
 
 plotcorr<- function(d) {
     maxs<-c(max(d$V2,na.rm=TRUE),max(d$V3,na.rm=TRUE))
+    #maxs<-c(2,2)
 
     bio1<-d$V2[d$V4=="bio"]
     bio2<-d$V3[d$V4=="bio"]
@@ -22,3 +23,20 @@ plotcorr<- function(d) {
     abline(v=1.05,lty=2,col=8)
 }
 plotcorr(dnw)
+
+# entropy vs kaks correlations
+ent.v.kaks<-read.table("/afs/psi.ch/project/bioinfo2/kaks/jose/plp/entr_vs_kaks.txt")
+ent.v.kaks<-read.table("/afs/psi.ch/project/bioinfo2/kaks/jose/dey/entr_vs_kaks.txt")
+
+ent.all<-d$V5[d$V3!="nopred" & d$V4!="nopred"]
+kaks.all<-d$V6[d$V3!="nopred" & d$V4!="nopred"]
+#ent.bio<-d$V5[d$V3=="bio" & d$V4=="bio"]
+#kaks.bio<-d$V6[d$V3=="bio" & d$V4=="bio"]
+#ent.xtal<-d$V5[d$V3=="xtal" & d$V4=="xtal"]
+#kaks.xtal<-d$V6[d$V3=="xtal" & d$V4=="xtal"]
+	
+plot(ent.all,kaks.all,xlim=c(0,3),ylim=c(0,3),xlab="entropy score", ylab="kaks score")
+points(ent.all[ent.all>1 & kaks.all<0.85],kaks.all[ent.all>1 & kaks.all<0.85],col=3)
+points(ent.all[kaks.all>0.85 & ent.all<1],kaks.all[kaks.all>0.85 & ent.all<1],col=2)
+abline(h=0.85,lty=2,col=8)
+abline(v=1.00,lty=2,col=8)
