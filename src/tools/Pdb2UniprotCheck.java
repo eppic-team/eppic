e7c0894ea5b8427ffed6dd46f7cb328e0b434f30
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2011-01-18 16:43:16 +0000

    New package for scripts that are not really core crk core.

--- src/tools/Pdb2UniprotCheck.java
+++ src/tools/Pdb2UniprotCheck.java
@@ -1,4 +1,4 @@
-package crk;
+package tools;
 
 import java.io.BufferedReader;
 import java.io.File;
--- src/tools/RescaleBfactors.java
+++ src/tools/RescaleBfactors.java
@@ -1,4 +1,4 @@
-package crk;
+package tools;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
