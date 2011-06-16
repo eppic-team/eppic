bf9c4b40621cb0cc500c58024ccadef6e4b36441
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2011-06-16 13:36:42 +0000

    Adapted to changes in CRK, now we don't have arrays anymore + nopred warnings is now just a string callReason. Not tested, most likely the web ui is not working at the moment

--- src/ch/systemsx/sybit/crkwebui/server/util/PDBModelConverter.java
+++ src/ch/systemsx/sybit/crkwebui/server/util/PDBModelConverter.java
@@ -24,7 +24,7 @@ public class PDBModelConverter
 			{
 				pdbScoreItem = new PDBScoreItem();
 				pdbScoreItem.setBioCutoff(pdbScore[0].getBioCutoff());
-				pdbScoreItem.setBsaToAsaCutoffs(pdbScore[0].getBsaToAsaCutoffs());
+				pdbScoreItem.setBsaToAsaCutoff(pdbScore[0].getBsaToAsaCutoff());
 				pdbScoreItem.setBsaToAsaRelaxStep(pdbScore[0].getBsaToAsaRelaxStep());
 				pdbScoreItem.setBsaToAsaSoftCutoff(pdbScore[0].getBsaToAsaSoftCutoff());
 				pdbScoreItem.setHomologsCutoff(pdbScore[0].getHomologsCutoff());
@@ -52,13 +52,13 @@ public class PDBModelConverter
 						interfaceItem.setArea(pdbScore[0].getInterfaceScoreMap().get(i).getInterfArea());
 						interfaceItem.setName(pdbScore[0].getInterfaceScoreMap().get(i).getFirstChainId() + "+" + 
 											  pdbScore[0].getInterfaceScoreMap().get(i).getSecondChainId());
-						interfaceItem.setSize1(pdbScore[0].getInterfaceScoreMap().get(i).getCoreSize1()[0]);
-						interfaceItem.setSize2(pdbScore[0].getInterfaceScoreMap().get(i).getCoreSize2()[0]);
+						interfaceItem.setSize1(pdbScore[0].getInterfaceScoreMap().get(i).getCoreSize1());
+						interfaceItem.setSize2(pdbScore[0].getInterfaceScoreMap().get(i).getCoreSize2());
 						interfaceItem.setNumHomologs1(pdbScore[0].getInterfaceScoreMap().get(i).getNumHomologs1());
 						interfaceItem.setNumHomologs2(pdbScore[0].getInterfaceScoreMap().get(i).getNumHomologs2());
 						interfaceItem.setOperator(pdbScore[0].getInterfaceScoreMap().get(i).getOperator());
 						interfaceItem.setWarnings(pdbScore[0].getInterfaceScoreMap().get(i).getWarnings());
-						interfaceItem.setNopredWarnings(pdbScore[0].getInterfaceScoreMap().get(i).getNopredWarnings());
+						interfaceItem.setCallReason(pdbScore[0].getInterfaceScoreMap().get(i).getCallReason());
 						pdbScoreItem.addInterfaceItem(interfaceItem);
 						
 						int j=0;
@@ -79,31 +79,31 @@ public class PDBModelConverter
 							interfaceScoreItem.setMethod(method);
 //							interfaceScoreItem.setPdbScoreItem(pdbScoreItem);
 							
-							interfaceScoreItem.setUnweightedRim1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRim1Scores()[0]);
-							interfaceScoreItem.setWeightedRim1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRim1Scores()[0]);
+							interfaceScoreItem.setUnweightedRim1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRim1Score());
+							interfaceScoreItem.setWeightedRim1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRim1Score());
 							
-							interfaceScoreItem.setUnweightedCore1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getCore1Scores()[0]);
-							interfaceScoreItem.setWeightedCore1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getCore1Scores()[0]);
+							interfaceScoreItem.setUnweightedCore1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getCore1Score());
+							interfaceScoreItem.setWeightedCore1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getCore1Score());
 							
-							interfaceScoreItem.setUnweightedRim2Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRim2Scores()[0]);
-							interfaceScoreItem.setWeightedRim2Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRim2Scores()[0]);
+							interfaceScoreItem.setUnweightedRim2Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRim2Score());
+							interfaceScoreItem.setWeightedRim2Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRim2Score());
 							
-							interfaceScoreItem.setUnweightedRim1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRim1Scores()[0]);
-							interfaceScoreItem.setWeightedRim1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRim1Scores()[0]);
+							interfaceScoreItem.setUnweightedRim1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRim1Score());
+							interfaceScoreItem.setWeightedRim1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRim1Score());
 							
-							interfaceScoreItem.setUnweightedCore2Scores(pdbScore[j].getInterfaceScoreMap().get(i).getCore2Scores()[0]);
-							interfaceScoreItem.setWeightedCore2Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getCore2Scores()[0]);
+							interfaceScoreItem.setUnweightedCore2Scores(pdbScore[j].getInterfaceScoreMap().get(i).getCore2Score());
+							interfaceScoreItem.setWeightedCore2Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getCore2Score());
 							
-							interfaceScoreItem.setUnweightedRatio1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRatio1Scores()[0]);
-							interfaceScoreItem.setWeightedRatio1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRatio1Scores()[0]);
+							interfaceScoreItem.setUnweightedRatio1Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRatio1Score());
+							interfaceScoreItem.setWeightedRatio1Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRatio1Score());
 							
-							interfaceScoreItem.setUnweightedRatio2Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRatio2Scores()[0]);
-							interfaceScoreItem.setWeightedRatio2Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRatio2Scores()[0]);
+							interfaceScoreItem.setUnweightedRatio2Scores(pdbScore[j].getInterfaceScoreMap().get(i).getRatio2Score());
+							interfaceScoreItem.setWeightedRatio2Scores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getRatio2Score());
 							
-							interfaceScoreItem.setUnweightedFinalScores(pdbScore[j].getInterfaceScoreMap().get(i).getFinalScores()[0]);
-							interfaceScoreItem.setWeightedFinalScores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getFinalScores()[0]);
+							interfaceScoreItem.setUnweightedFinalScores(pdbScore[j].getInterfaceScoreMap().get(i).getFinalScore());
+							interfaceScoreItem.setWeightedFinalScores(pdbScore[j + 1].getInterfaceScoreMap().get(i).getFinalScore());
 							
-							interfaceScoreItem.setCall(pdbScore[j].getInterfaceScoreMap().get(i).getCalls()[0].getName());
+							interfaceScoreItem.setCall(pdbScore[j].getInterfaceScoreMap().get(i).getCall().getName());
 							
 							InterfaceScoreItemKey interfaceScoreItemKey = new InterfaceScoreItemKey();
 							interfaceScoreItemKey.setInterfaceId(i);
