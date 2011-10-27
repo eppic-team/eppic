2cecd296a7903a925f25437c9dbce82a28b49b8f
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2011-10-27 07:50:05 +0000

    Changed combined prediction strategy slightly

--- src/crk/predictors/CombinedPredictor.java
+++ src/crk/predictors/CombinedPredictor.java
@@ -37,27 +37,66 @@ public class CombinedPredictor implements InterfaceTypePredictor {
 			call = CallType.BIO;
 		}
 		else {
+			// STRATEGY 1: consensus, when no evolution take geometry, when no consensus take evol
 			int[] counts = countCalls();
-			// 2 bio calls
+			// 1) 2 bio calls
 			if (counts[0]>=2) {
 				callReason = "BIO consensus ("+counts[0]+" votes)";
 				call = CallType.BIO;
 			} 
-			// 2 xtal calls
+			// 2) 2 xtal calls
 			else if (counts[1]>=2) {
 				callReason = "XTAL consensus ("+counts[1]+" votes)";
 				call = CallType.CRYSTAL;
 			}
-			// 2 nopreds (necessarily from the evol methods): we take geometry as the call
+			// 3) 2 nopreds (necessarily from the evol methods): we take geometry as the call
 			else if (counts[2]==2) {
 				callReason = "Prediction purely geometrical (no evolutionary prediction could be made): "+gp.getCallReason();
 				call = gp.getCall();
 			}
-			// 1 nopred (an evol method), 1 xtal, 1 bio
+			// 4) 1 nopred (an evol method), 1 xtal, 1 bio
 			else {
-				callReason ="No consensus. Z-score "+zp.getCall().getName()+", core/rim "+rp.getCall().getName()+". Taking geometrical call as final: "+gp.getCallReason();
-				call = gp.getCall();
+				// sub-strategy a) take geometry call
+				//callReason ="No consensus. Z-score "+zp.getCall().getName()+", core/rim "+rp.getCall().getName()+". Taking geometrical call as final: "+gp.getCallReason();
+				//call = gp.getCall();
+				// sub-strategy b) take evol call
+				InterfaceTypePredictor validPred = null;
+				if (rp.getCall()!=CallType.NO_PREDICTION) validPred = rp;
+				else validPred = zp;
+				callReason = "No consensus. Z-score "+zp.getCall().getName()+", core/rim "+rp.getCall().getName()+". Taking evol call as final: "+validPred.getCallReason();
+				call = validPred.getCall();
 			}
+//			// STRATEGY 2: trust more evolution when we can
+//			// 1) there is evolutionary prediction from both methods
+//			if (rp.getCall()!=CallType.NO_PREDICTION && zp.getCall()!=CallType.NO_PREDICTION) {
+//				if (rp.getCall()==zp.getCall()) {
+//					call = rp.getCall();
+//					callReason = "Consensus of evol predictions";
+//				} else {
+//					// each evol prediction votes in a different direction, geometry has to give the last word (so in total 2 votes go for it)
+//					call = gp.getCall();
+//					callReason = "One evolutionary measure and geometry agree";
+//				}
+//			// 2) there's no evol predictions at all
+//			} else if (rp.getCall()==CallType.NO_PREDICTION && zp.getCall()==CallType.NO_PREDICTION) {
+//				call = gp.getCall();
+//				callReason = "Couldn't do evol predictions. Taking geometry as final prediction";				
+//			}
+//			// 3) only one of the evol predicts
+//			else if (rp.getCall()==CallType.NO_PREDICTION || zp.getCall()==CallType.NO_PREDICTION) {
+//				InterfaceTypePredictor validPred = null;
+//				if (rp.getCall()!=CallType.NO_PREDICTION) validPred = rp;
+//				else validPred = zp;
+//				
+//				if (gp.getCall()==validPred.getCall()) {
+//					call = validPred.getCall();
+//					callReason = "One evolutionary could not predict and the other evol measure and geometry agree";
+//				} else {
+//					// no agreement: we trust evolution
+//					call = validPred.getCall();
+//					callReason = "Only one evol measure predicts and disagrees with geometry: taking evol prediction";
+//				}
+//			}
 		}
 	
 		return call;
