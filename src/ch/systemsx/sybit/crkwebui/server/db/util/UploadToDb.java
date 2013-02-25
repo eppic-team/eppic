fa54e30f8f7a8220d9afad05880d15246c214f9e
Nikhil Biyani <nikhilbiyani@gmail.com>
2013-02-25 10:40:15 +0000

    Update for handling the Database. New functions added for handling user-jobs!

--- src/ch/systemsx/sybit/crkwebui/server/db/util/UploadToDb.java
+++ src/ch/systemsx/sybit/crkwebui/server/db/util/UploadToDb.java
@@ -7,127 +7,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.TypedQuery;
-
-import model.JobDB;
 import model.PDBScoreItemDB;
-import ch.systemsx.sybit.crkwebui.server.db.EntityManagerHandler;
-import ch.systemsx.sybit.crkwebui.shared.model.InputType;
-import ch.systemsx.sybit.crkwebui.shared.model.StatusOfJob;
 
 public class UploadToDb {
 
-	private static void addToDB(PDBScoreItemDB pdbScoreItem){
-		Date submissionDate = new Date();
-		EntityManager entityManager = EntityManagerHandler.getEntityManager();
-		try
-		{
-			entityManager.getTransaction().begin();
-			entityManager.persist(pdbScoreItem);
+	private static DBHandler dbh = new DBHandler();
 	
-			String pdbCode = pdbScoreItem.getPdbName();
-
-			JobDB job = new JobDB();
-			job.setJobId(pdbCode);
-			job.setEmail(null);
-			job.setInput(pdbCode);
-			job.setIp("localhost");
-			job.setStatus(StatusOfJob.FINISHED.getName());
-			job.setSubmissionDate(submissionDate);
-			job.setInputType(InputType.PDBCODE.getIndex());
-			job.setSubmissionId("-1");
-
-			pdbScoreItem.setJobItem(job);
-			job.setPdbScoreItem(pdbScoreItem);
-			entityManager.persist(job);
-			System.out.print(" Updating.. ");
-			entityManager.getTransaction().commit();			
-		}
-		catch(Throwable e)
-		{
-			e.printStackTrace();
-
-			try
-			{
-				entityManager.getTransaction().rollback();
-			}
-			catch(Throwable t)
-			{
-				t.printStackTrace();
-			}
-
-		}
-		finally
-		{
-			try
-			{
-				entityManager.close();
-			}
-			catch(Throwable t)
-			{
-				t.printStackTrace();
-			}
-			System.out.println();
-		}
-	}
-	
-	private static void removefromDB(String pdbID){
-		EntityManager entityManager = EntityManagerHandler.getEntityManager();
-		entityManager.getTransaction().begin();
-		
-		String queryJobstr = "FROM " + JobDB.class.getName() + " WHERE jobId='" + pdbID +"'";
-		TypedQuery<JobDB> queryJob = entityManager.createQuery(queryJobstr, JobDB.class);
-						
-		List<JobDB> queryJobList = queryJob.getResultList();
-		int querySize = queryJobList.size();
-		
-		if(querySize>0){
-			System.out.print(" Already present (" + querySize + " times) Removing ");
-			for(JobDB itemJobDB:queryJobList){
-				Long itemUid = itemJobDB.getUid();
-				String queryPDBstr = "FROM " + PDBScoreItemDB.class.getName() + " WHERE jobItem_uid='" + itemUid +"'";
-				TypedQuery<PDBScoreItemDB> queryPDB = entityManager.createQuery(queryPDBstr, PDBScoreItemDB.class);
-				List<PDBScoreItemDB> queryPDBList = queryPDB.getResultList();
-				if( queryPDBList != null)
-				{
-					for(PDBScoreItemDB itemPDB : queryPDBList)
-					{
-						entityManager.remove(itemPDB);
-					}
-				}
-				entityManager.remove(itemJobDB);
-			}
-		}
-		else System.out.print(" Not present in Data Base ");
-		entityManager.getTransaction().commit();
-	}
-	
-	private static boolean checkfromDB(String pdbID){
-		EntityManager entityManager = EntityManagerHandler.getEntityManager();
-		
-		String queryJobstr = "FROM " + JobDB.class.getName() + " WHERE jobId='" + pdbID +"'";
-		TypedQuery<JobDB> queryJob = entityManager.createQuery(queryJobstr, JobDB.class);
-						
-		List<JobDB> queryJobList = queryJob.getResultList();
-		int querySize = queryJobList.size();
-		
-		if(querySize>0) {
-			System.out.print(" Already present (" + querySize + " times) ");
-			return true;
-		}
-		else {
-			System.out.print(" Not present in DB ");
-			return false;
-		}
-	}
 	
 	/**
 	 * @param args
@@ -138,30 +27,24 @@ public class UploadToDb {
 		
 		String help = 
 				"Usage: UploadToDB\n" +
-				"Uploads a set of id's to Data Base \n" +
-				" -d <dir>  	: Directory of eppic output files \n" +
-				" [-f <file>] 	: File specifying the directories to be used from output directory \n" +
-				"                  Default: Uses all files in the directory as \n" +
-				" [-F]          : Force everything to be continued without any breaks\n" +
-				" TYPE of ID's to be considered\n" +
-				" 	[-p]        : (Default option) Uses PDB IDs only for operations \n" +
-				" 	[-j]        : Uses User-Job-ID's only for operations \n" +
+				"Uploads a set of eppic output files to Data Base \n" +
+				" -d <dir>  	: Root Directory of eppic output files with subdirectories as pdb names \n" +
+				" [-f <file>] 	: File specifying the sub-directories to be used from output directory \n" +
+				"                 (Default: Uses all files in the root directory) \n" +
 				" OPERATION MODE\n" +
-				" 	[-n]        : (Default mode) Inserts the entries which are not in the database\n" +
-				" 	[-e]        : Everything chosen is inserted, deletes previous entries if present\n " +
-				"	[-r]        : Removes the specified files\n";
+				" (In conflicting cases, options you specify later override the earlier ones)\n" +
+				" 	[-i]        : (Default mode) Inserts only the entries which are not in the database\n" +
+				" 	[-F]        : Forces everything chosen to be inserted, deletes previous entries if present\n " +
+				"	[-r]        : Removes the specified files from Database\n";
 
 		File jobDirectoriesRoot = null;
 		File choosefromFile = null;
-		boolean force = false;
 		boolean choosefrom = false;
-		boolean typePDB = true;
-		boolean typeJob = false;
 		boolean modeNew = true;
 		boolean modeEverything = false;
 		boolean modeRemove = false;
 
-		Getopt g = new Getopt("UploadToDB", args, "d:f:Fpjnerh?");
+		Getopt g = new Getopt("UploadToDB", args, "d:f:iFrh?");
 		int c;
 		while ((c = g.getopt()) != -1) {
 			switch(c){
@@ -172,23 +55,12 @@ public class UploadToDb {
 				choosefrom = true;
 				choosefromFile = new File(g.getOptarg());
 				break;
-			case 'F':
-				force = true;
-				break;
-			case 'p':
-				typePDB = true;
-				typeJob = false;
-				break;
-			case 'j':
-				typeJob = true;
-				typePDB = false;
-				break;
-			case 'n':
+			case 'i':
 				modeNew = true;
 				modeEverything = false;
 				modeRemove = false;
 				break;
-			case 'e':
+			case 'F':
 				modeEverything = true;
 				modeNew = false;
 				modeRemove = false;
@@ -210,19 +82,13 @@ public class UploadToDb {
 		}
 
 		if (jobDirectoriesRoot == null || ! jobDirectoriesRoot.isDirectory() ){
-			System.err.println("Hey Jim, Output Directory not specified correctly! ");
+			System.err.println("\n\nHey Jim, Output Directory not specified correctly! \n");
 			System.err.println(help);
 			System.exit(1);
 		}
 		
 		if ( choosefrom && choosefromFile == null ){
-			System.err.println("Hey Jim, Option -f not specified correctly! ");
-			System.err.println(help);
-			System.exit(1);
-		}
-		
-		if ( typePDB && typeJob ){
-			System.err.println("Hey Jim, Combinations of TYPE ( -p/-j/-a ) not acceptable! ");
+			System.err.println("\n\nHey Jim, Option -f not specified correctly! \n");
 			System.err.println(help);
 			System.exit(1);
 		}
@@ -231,35 +97,12 @@ public class UploadToDb {
 		if(!( ( (modeNew) && !(modeEverything) && !(modeRemove) ) || 
 			  (!(modeNew) &&   modeEverything &&  !(modeRemove) ) ||
 			  (!(modeNew) && !(modeEverything) &&   modeRemove  ) ) ){
-			System.err.println("Hey Jim, Combinations of MODE ( -n/-e/-r ) not acceptable! ");
+			System.err.println("\n\nHey Jim, Combinations of MODE ( -n / -F / -r ) not acceptable! \n");
 			System.err.println(help);
 			System.exit(1);
 		}
 		
-		// -j (User-Job Mode) works with remove only!!
-		if( (typeJob && modeNew) || (typeJob && modeEverything) ){
-			System.err.println("Hey Jim, -j mode works with -r mode only");
-			System.err.println(help);
-			System.exit(1);
-		}
-		
-		// Check if user really want to remove user-jobs
-		if( typeJob && modeRemove && !(force)){
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("You are trying to remove user-jobs from database. They cannot be added back." +
-					" Are you sure to continue (type 'yes' to continue)? ");
-			try {
-				String answer = br.readLine();
-				if (!answer.equals("yes")) {
-					System.err.println("Exiting, Please carefully check the options you are trying to use!");
-					System.err.println(help);
-					System.exit(1);
-				}
-			} catch (IOException e) {
-				System.err.println("Error while trying to read standard input");
-				System.exit(1);
-			}
-		}
+		// Get the Directories to be processed
 		File[] jobsDirectories;
 		if(choosefrom == true){
 			BufferedReader choosefromFileRead = new BufferedReader(new FileReader(choosefromFile));
@@ -277,21 +120,24 @@ public class UploadToDb {
 			jobsDirectories = jobDirectoriesRoot.listFiles();
 		}
 		
+		//Print the MODE of usage
+		if(modeNew) System.out.println("\n\nMODE SELECTED: Insert New Entries\n");
+		if(modeEverything) System.out.println("\n\nMODE SELECTED: Force Insert, which will insert everything in DB\n");
+		if(modeRemove) System.out.println("\n\nMODE SELECTED: Remove entried from DB\n");
+		
+		// Start the Process
 		for (File jobDirectory : jobsDirectories)
-		{			
+		{	
+			//Check if it really is a directory
 			if (!jobDirectory.isDirectory()){
-				if(choosefrom == true){
-					System.err.println("Warning: "+jobDirectory.getName()+" specified in JobsFile, but directory is not present, Skipping");
-					continue;
-				}
-				else continue;
+				if(choosefrom == true)	System.err.println("Warning: "+jobDirectory.getName()+" specified in JobsFile, " +
+															"but directory is not present, Skipping");
+				continue;
 			}
 			
-			if (typeJob == true && !jobDirectory.getName().matches("^[a-zA-Z]\\w{29}$")){ 
-				System.out.println("Skipping " + jobDirectory.getName()); 
-				continue; }
-			if (typePDB == true && !jobDirectory.getName().matches("^\\d\\w\\w\\w$")){
-				System.out.println("Skipping " + jobDirectory.getName());
+			//Check for 4 letter code directories starting with a number
+			if (!jobDirectory.getName().matches("^\\d\\w\\w\\w$")){
+				System.out.println("Skipping Directory: " + jobDirectory.getName());
 				continue; 
 			}
 			
@@ -301,60 +147,60 @@ public class UploadToDb {
 				
 				long start = System.currentTimeMillis();
 				
-				String currentPDB = null;
+				String currentPDB = jobDirectory.getName();
 				
+								
+				// Get the PDB-Score Item to be read
+				File webuiFile = new File(jobDirectory, currentPDB + ".webui.dat");
+				boolean isWuiPresent = webuiFile.isFile();
 				
-				if (typePDB){
-					currentPDB = jobDirectory.getName();
+				Object toAdd = null;
+				
+				if(isWuiPresent) {
+					ObjectInputStream in = new ObjectInputStream(new FileInputStream(webuiFile));
+					PDBScoreItemDB pdbScoreItem = (PDBScoreItemDB)in.readObject();
+					in.close();
+					
+					toAdd = pdbScoreItem;
 				}
-				
-				// Get pdbName in -j type
-				if (typeJob){
-					String getPDBQueryStr = "SELECT input FROM " + JobDB.class.getName() + " where jobId='"+jobDirectory.getName() +"'";
-					TypedQuery<String> getPDBQuery = EntityManagerHandler.getEntityManager().createQuery(getPDBQueryStr, String.class);
-					try{
-						currentPDB = getPDBQuery.getSingleResult();
-					}
-					catch(NoResultException ex){
-						System.out.print(": Not present in Database.. Skipping..\n");
-						continue;
-					}
+				else {
+					toAdd = currentPDB;
 				}
-				
-				
-				ObjectInputStream in = new ObjectInputStream(new FileInputStream(
-						new File(jobDirectory, currentPDB + ".webui.dat")));
-				PDBScoreItemDB pdbScoreItem = (PDBScoreItemDB)in.readObject();
-				in.close();
-				
-				//MODE REMOVE
-				if (modeRemove) {
-					if (typeJob) removefromDB(jobDirectory.getName());
-					else removefromDB(pdbScoreItem.getPdbName());
-				}
-				
+					
 				//MODE FORCE
 				if (modeEverything) {
-					removefromDB(pdbScoreItem.getPdbName());
-					addToDB(pdbScoreItem);
+					boolean ifRemoved = dbh.removefromDB(currentPDB);
+					if (ifRemoved) System.out.print(" Found.. Removing and Updating.. ");
+					else System.out.print(" Not Found.. Adding.. ");
+					if(isWuiPresent) dbh.addToDB((PDBScoreItemDB) toAdd);
+					else dbh.addToDB((String) toAdd);
+					//continue;
 				}
 				
 				//MODE NEW INSERT
 				if (modeNew){
-					boolean isPresent = checkfromDB(pdbScoreItem.getPdbName());
-					if(!isPresent) addToDB(pdbScoreItem);
-					else System.out.print(" Skipping ");
+					boolean isPresent = dbh.checkfromDB(currentPDB);
+					if(!isPresent){
+						System.out.print(" Not Present.. Adding.. ");
+						if(isWuiPresent) dbh.addToDB((PDBScoreItemDB) toAdd);
+						else dbh.addToDB((String) toAdd);
+					}
+					else System.out.print(" Already Present.. Skipping.. ");
+					//continue;
+				}
+				
+				//MODE REMOVE
+				if (modeRemove) {
+					boolean ifRemoved = dbh.removefromDB(currentPDB);
+					if (ifRemoved) System.out.print(" Removed.. ");
+					else System.out.print(" Not Found.. ");
+					//continue;
 				}
 				
 				long end = System.currentTimeMillis();
 				System.out.print(((end-start)/1000)+"s\n");
 				
 			}
-			catch(IOException t)
-			{
-				System.err.println("Failed to read file "+t.getMessage());
-			}
-
 			catch(Throwable t)
 			{
 				t.printStackTrace();
