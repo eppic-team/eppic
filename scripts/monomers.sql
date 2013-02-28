6d059e419e4eff9b5a17beedcdf95b7f54ebc6e0
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2013-02-28 17:01:34 +0000

    Old changes in schema not committed before

--- scripts/monomers.sql
+++ scripts/monomers.sql
@@ -1,10 +1,14 @@
 CREATE TABLE monomers (
   pdb_code char(4),
+  id int,
   interf_members varchar(5),
   transf varchar(40),
   area float,
   clashes int,
   resolution float,
   rfree float,
-  rsym float
-)
+  rsym float,
+  core1 int,
+  core2 int
+);
+
