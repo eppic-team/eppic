c55278d2134a3e69cb2dbdabeff22c9495571ddf
Nikhil Biyani <nikhilbiyani@gmail.com>
2013-10-29 14:07:07 +0000

    Deleted extra code and merged HomolgsInfoPanel and SequenceInfoPanel

--- src/ch/systemsx/sybit/crkwebui/client/results/gui/panels/SequenceInfoPanel.java
+++ src/ch/systemsx/sybit/crkwebui/client/results/gui/panels/SequenceInfoPanel.java
@@ -7,10 +7,14 @@ import ch.systemsx.sybit.crkwebui.client.commons.appdata.AppPropertiesManager;
 import ch.systemsx.sybit.crkwebui.client.commons.appdata.ApplicationContext;
 import ch.systemsx.sybit.crkwebui.client.commons.events.ApplicationWindowResizeEvent;
 import ch.systemsx.sybit.crkwebui.client.commons.events.HideAllWindowsEvent;
+import ch.systemsx.sybit.crkwebui.client.commons.events.ShowAlignmentsEvent;
 import ch.systemsx.sybit.crkwebui.client.commons.events.ShowQueryWarningsEvent;
 import ch.systemsx.sybit.crkwebui.client.commons.gui.data.TooltipXPositionType;
 import ch.systemsx.sybit.crkwebui.client.commons.gui.data.TooltipYPositionType;
 import ch.systemsx.sybit.crkwebui.client.commons.gui.images.ImageWithTooltip;
+import ch.systemsx.sybit.crkwebui.client.commons.gui.links.EmptyLinkWithTooltip;
+import ch.systemsx.sybit.crkwebui.client.commons.gui.links.ImageLinkWithTooltip;
+import ch.systemsx.sybit.crkwebui.client.commons.gui.links.LinkWithTooltip;
 import ch.systemsx.sybit.crkwebui.client.commons.handlers.ApplicationWindowResizeHandler;
 import ch.systemsx.sybit.crkwebui.client.commons.handlers.HideAllWindowsHandler;
 import ch.systemsx.sybit.crkwebui.client.commons.handlers.ShowQueryWarningsHandler;
@@ -18,14 +22,19 @@ import ch.systemsx.sybit.crkwebui.client.commons.managers.EventBusManager;
 import ch.systemsx.sybit.crkwebui.client.commons.util.EscapedStringGenerator;
 import ch.systemsx.sybit.crkwebui.shared.model.HomologsInfoItem;
 import ch.systemsx.sybit.crkwebui.shared.model.PDBScoreItem;
+import ch.systemsx.sybit.crkwebui.shared.model.QueryWarningItem;
 
 import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.core.Template;
+import com.extjs.gxt.ui.client.event.BaseEvent;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.widget.Label;
 import com.extjs.gxt.ui.client.widget.form.FieldSet;
 import com.extjs.gxt.ui.client.widget.layout.ColumnLayout;
 import com.extjs.gxt.ui.client.widget.tips.ToolTip;
 import com.extjs.gxt.ui.client.widget.tips.ToolTipConfig;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlexTable;
@@ -162,7 +171,7 @@ public class SequenceInfoPanel extends FieldSet
 	ArrayList<List<Widget>> homologsInfoPanels = new ArrayList<List<Widget>>();
 	for(int i=0; i<homologsStrings.size(); i++)
 	{
-	    homologsInfoPanels.add(HomologsInfoPanel.generateHomologsInfoPanelItems(pdbScoreItem.getJobId(),
+	    homologsInfoPanels.add(generateHomologsInfoPanelItems(pdbScoreItem.getJobId(),
 		    homologsStrings.get(i),
 		    pdbScoreItem.getPdbName()));
 	};
@@ -175,6 +184,168 @@ public class SequenceInfoPanel extends FieldSet
      */
     public ToolTip getQueryWarningsTooltip() {
 	return queryWarningsTooltip;
+    }
+    
+    /**
+     * Creates template for query warnings tooltip.
+     * @param warnings list of warnings
+     * @return template for query warnings
+     */
+    private static String generateHomologsNoQueryMatchTemplate(List<QueryWarningItem> warnings)
+    {
+	String warningsList = "<div><ul class=\"eppic-tooltip-list\">";
+
+	for(QueryWarningItem warning : warnings)
+	{
+	    if((warning.getText() != null) &&
+		    (!warning.getText().equals("")))
+	    {
+		warningsList += "<li>" + EscapedStringGenerator.generateSanitizedString(warning.getText()) + "</li>";
+	    }
+	}
+
+	warningsList += "</ul></div>";
+
+	return warningsList;
+    }
+    
+    /**
+     * Method to generate homologs in the sequence info panel
+     * @param selectedJobId
+     * @param homologsInfoItem
+     * @param pdbName
+     * @return
+     */
+    public static List<Widget>  generateHomologsInfoPanelItems(final String selectedJobId,
+	    final HomologsInfoItem homologsInfoItem,
+	    final String pdbName)
+	    {
+
+	if(homologsInfoItem.isHasQueryMatch())
+	{
+	    return createHomologsPanelItemsIfQueryMatch(selectedJobId, homologsInfoItem, pdbName);
+	}
+	else
+	{
+	    return createHomologsPanelItemsIfNoQueryMatch(homologsInfoItem);
+	}
+	    }
+
+    private static List<Widget> createHomologsPanelItemsIfNoQueryMatch(final HomologsInfoItem homologsInfoItem)
+    {
+	ArrayList<Widget> items = new ArrayList<Widget>();
+	
+	String chainStr = EscapedStringGenerator.generateEscapedString(homologsInfoItem.getChains());
+	String chainHintStr = AppPropertiesManager.CONSTANTS.homologs_panel_uniprot_no_query_match_hint();
+	if(chainStr.length() > 13){
+		chainStr = chainStr.substring(0,12)+",..)";
+		chainHintStr = "<b>Chain " + EscapedStringGenerator.generateEscapedString(homologsInfoItem.getChains())+
+				"</b><br>"+AppPropertiesManager.CONSTANTS.homologs_panel_chains_hint();;
+	}
+	
+	final EmptyLinkWithTooltip chainsLink = new EmptyLinkWithTooltip("Chain " + EscapedStringGenerator.generateEscapedString(chainStr), 
+		chainHintStr,
+		ApplicationContext.getWindowData(), 
+		0);
+	
+	chainsLink.addStyleName("eppic-action");
+
+	chainsLink.addListener(Events.OnClick, new Listener<BaseEvent>() {
+
+	    @Override
+	    public void handleEvent(BaseEvent be) {
+
+		EventBusManager.EVENT_BUS.fireEvent(new ShowQueryWarningsEvent(generateHomologsNoQueryMatchTemplate(homologsInfoItem.getQueryWarnings()),
+			chainsLink.getAbsoluteLeft() + chainsLink.getWidth(),
+			chainsLink.getAbsoluteTop() + chainsLink.getHeight() + 10));
+	    }
+
+	});
+
+	items.add(chainsLink);
+	return items;
+    }
+
+    private static List<Widget> createHomologsPanelItemsIfQueryMatch(final String selectedJobId,
+	    final HomologsInfoItem homologsInfoItem,
+	    final String pdbName) {
+	ArrayList<Widget> items = new ArrayList<Widget>();
+	
+	String chainStr = EscapedStringGenerator.generateEscapedString(homologsInfoItem.getChains());
+	String chainHintStr = AppPropertiesManager.CONSTANTS.homologs_panel_chains_hint();
+	if(chainStr.length() > 13){
+		chainStr = chainStr.substring(0,12)+",..)";
+		chainHintStr = "<b>Chain " + EscapedStringGenerator.generateEscapedString(homologsInfoItem.getChains())+
+				"</b><br>"+AppPropertiesManager.CONSTANTS.homologs_panel_chains_hint();;
+	}
+	
+	final EmptyLinkWithTooltip chainsLink = new EmptyLinkWithTooltip("Chain " + EscapedStringGenerator.generateEscapedString(chainStr), 
+		chainHintStr,
+		ApplicationContext.getWindowData(), 
+		0);
+
+	chainsLink.addStyleName("eppic-action");
+
+	chainsLink.addListener(Events.OnClick, new Listener<BaseEvent>() {
+
+	    @Override
+	    public void handleEvent(BaseEvent be) {
+		EventBusManager.EVENT_BUS.fireEvent(new ShowAlignmentsEvent(
+			homologsInfoItem, 
+			pdbName,
+			chainsLink.getAbsoluteLeft() + chainsLink.getWidth(),
+			chainsLink.getAbsoluteTop() + chainsLink.getHeight() + 10));
+	    }
+
+	});
+
+	items.add(chainsLink);
+
+	LinkWithTooltip uniprotIdLabel = new LinkWithTooltip(" (" + EscapedStringGenerator.generateEscapedString(homologsInfoItem.getUniprotId()) + ") ", 
+		AppPropertiesManager.CONSTANTS.homologs_panel_uniprot_hint(),
+		ApplicationContext.getWindowData(), 
+		0, 
+		ApplicationContext.getSettings().getUniprotLinkUrl() + homologsInfoItem.getUniprotId());
+	uniprotIdLabel.addStyleName("eppic-external-link");
+	items.add(uniprotIdLabel);
+
+	int nrOfHomologs = homologsInfoItem.getNumHomologs();
+	String nrOfHomologsText = String.valueOf(nrOfHomologs) + " homolog";
+
+	if(nrOfHomologs > 1)
+	{
+	    nrOfHomologsText += "s";
+	}
+
+	String alignmentId = homologsInfoItem.getChains().substring(0, 1);
+	String downloadLink = GWT.getModuleBaseURL() + "fileDownload?type=fasta&id=" + selectedJobId + "&alignment=" + alignmentId; 
+
+	LinkWithTooltip nrHomologsLabel = new LinkWithTooltip(nrOfHomologsText, 
+		AppPropertiesManager.CONSTANTS.homologs_panel_nrhomologs_hint(),
+		ApplicationContext.getWindowData(), 
+		0, 
+		downloadLink);
+
+	nrHomologsLabel.addStyleName("eppic-internal-link");
+	items.add(nrHomologsLabel);
+
+	String downloadPseLink = GWT.getModuleBaseURL() + 
+		"fileDownload?type=entropiespse&id=" + selectedJobId + "&alignment=" + alignmentId; 
+
+	String colorPseIconImgSrc = 
+		ApplicationContext.getSettings().getResultsLocation() + 
+		selectedJobId+"/"+
+		pdbName +"."+alignmentId+".entropies.png";
+	//String colorPseIconImgSrc = "resources/icons/entropies_pse_icon.png";
+
+	ImageLinkWithTooltip colorPseImg = 
+		new ImageLinkWithTooltip(colorPseIconImgSrc, 14, 14, 
+			AppPropertiesManager.CONSTANTS.homologs_panel_entropiespse_hint(), 
+			ApplicationContext.getWindowData(), 
+			0, 
+			downloadPseLink);
+	items.add(colorPseImg);
+	return items;
     }
 
     /**
