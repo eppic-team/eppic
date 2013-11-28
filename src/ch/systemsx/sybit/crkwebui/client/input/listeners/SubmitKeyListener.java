73537479d8c0cee9f259edb09bc98df859d3a179
Bela Hullar <hullarb@gmail.com>
2013-11-28 15:51:24 +0000

    updating value on enter

--- src/ch/systemsx/sybit/crkwebui/client/input/listeners/SubmitKeyListener.java
+++ src/ch/systemsx/sybit/crkwebui/client/input/listeners/SubmitKeyListener.java
@@ -3,9 +3,11 @@ package ch.systemsx.sybit.crkwebui.client.input.listeners;
 import ch.systemsx.sybit.crkwebui.client.commons.events.SubmitJobEvent;
 import ch.systemsx.sybit.crkwebui.client.commons.managers.EventBusManager;
 
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
+import com.sencha.gxt.widget.core.client.form.TextField;
+import com.sencha.gxt.widget.core.client.form.ValueBaseField;
 
 /**
  * Submit job listener.
@@ -15,11 +17,17 @@ public class SubmitKeyListener implements KeyDownHandler
 	/**
 	 * Fires submit job event when enter pressed.
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void onKeyDown(KeyDownEvent event) {
 		if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
 		{
-			EventBusManager.EVENT_BUS.fireEvent(new SubmitJobEvent());
+		    if(event.getSource() instanceof ValueBaseField) {
+			@SuppressWarnings("rawtypes")
+			ValueBaseField valueBaseField = (ValueBaseField)event.getSource();
+			valueBaseField.setValue(valueBaseField.getCurrentValue());
+		    }
+		    EventBusManager.EVENT_BUS.fireEvent(new SubmitJobEvent());
 		}
 	}
 
