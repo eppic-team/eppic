cbc61837f9835079374240465fa1e8194de44781
Nikhil Biyani <nikhilbiyani@gmail.com>
2014-02-11 10:56:33 +0100

    Bugfixes while uploading user jobs:
    - Better Error handling
    - Truncation of extension in pdb codes

--- src/main/java/ch/systemsx/sybit/crkwebui/server/db/util/DBHandler.java
+++ src/main/java/ch/systemsx/sybit/crkwebui/server/db/util/DBHandler.java
@@ -325,7 +325,11 @@ public class DBHandler {
 		criteriaQuery.where(condition);
 
 		String pdbID = entityManager.createQuery(criteriaQuery).getSingleResult();
-		if(pdbID.endsWith(".pdb") || pdbID.endsWith(".cif")) pdbID = pdbID.substring(0, pdbID.length()-4);
+		int lastPeriodPos = pdbID.lastIndexOf('.');
+		if (lastPeriodPos >= 0)
+	    {
+			pdbID = pdbID.substring(0, lastPeriodPos);
+	    }
 		
 		File webuiFile = new File(jobDir, pdbID + ".webui.dat");
 		
