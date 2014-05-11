003487f6b0ae654b2d1cc2dc600e6d8a5bc17561
josemduarte <jose.m.duarte@gmail.com>
2014-05-11 23:45:44 +0200

    Some improvements in lattice overlap calculations

--- src/main/java/eppic/db/InterfaceTools.java
+++ src/main/java/eppic/db/InterfaceTools.java
@@ -1,8 +1,10 @@
 package eppic.db;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import edu.uci.ics.jung.graph.util.Pair;
 import eppic.model.ChainClusterDB;
 import eppic.model.InterfaceClusterDB;
 import eppic.model.InterfaceDB;
@@ -67,10 +69,10 @@ public class InterfaceTools {
 			
 			boolean match = false; 
 			
-			int seqClusterId1 = getChainClusterId(chainCluster1, seqClusterLevel);
+			int seqClusterId1 = getSeqClusterId(chainCluster1, seqClusterLevel);
 			
 			for (ChainClusterDB chainCluster2:pdb2.getChainClusters()) {
-				int seqClusterId2 = getChainClusterId(chainCluster2, seqClusterLevel);
+				int seqClusterId2 = getSeqClusterId(chainCluster2, seqClusterLevel);
 				if (seqClusterId1==seqClusterId2) {
 					match = true;
 					break;
@@ -83,7 +85,7 @@ public class InterfaceTools {
 		return true;
 	}
 	
-	public static int getChainClusterId(ChainClusterDB chainCluster, SeqClusterLevel seqClusterLevel) {
+	public static int getSeqClusterId(ChainClusterDB chainCluster, SeqClusterLevel seqClusterLevel) {
 		SeqClusterDB seqCluster = chainCluster.getSeqCluster();
 		int seqClusterId = -1;
 		switch (seqClusterLevel) {
@@ -111,5 +113,79 @@ public class InterfaceTools {
 
 		}
 		return seqClusterId;
+	}
+	
+	public static double[][] calcLatticeOverlapMatrix(PdbInfoDB pdb1, PdbInfoDB pdb2) {
+		
+		HashMap<String,ChainClusterDB> lookup1 = getChainIdLookup(pdb1);
+		HashMap<String,ChainClusterDB> lookup2 = getChainIdLookup(pdb2);
+		
+		int interfCount1 = getNumInterfacesAboveArea(pdb1, InterfaceTools.MIN_AREA_LATTICE_COMPARISON);
+		int interfCount2 = getNumInterfacesAboveArea(pdb2, InterfaceTools.MIN_AREA_LATTICE_COMPARISON);
+	
+		double[][] matrix = new double[interfCount1][interfCount2];
+
+		for (InterfaceDB interf1 : getInterfacesAboveArea(pdb1, InterfaceTools.MIN_AREA_LATTICE_COMPARISON)) {
+			for (InterfaceDB interf2 : getInterfacesAboveArea(pdb2, InterfaceTools.MIN_AREA_LATTICE_COMPARISON)) {
+				
+				if (! isSameContent(interf1, interf2, lookup1, lookup2)) {
+					continue; // the matrix value will remain 0
+				}
+				double co = calcInterfaceOverlap(interf1, interf2);
+				matrix[interf1.getInterfaceId()-1][interf2.getInterfaceId()-1] = co;
+			}
+		}	
+		
+		return matrix;
+	}
+	
+	private static boolean isSameContent(InterfaceDB interf1, InterfaceDB interf2, 
+			HashMap<String,ChainClusterDB> lookup1, HashMap<String,ChainClusterDB> lookup2) {
+		
+		String chain1i = interf1.getChain1();
+		String chain1j = interf1.getChain2();
+		String chain2i = interf2.getChain1();
+		String chain2j = interf2.getChain2();
+
+		ChainClusterDB c1i = lookup1.get(chain1i);
+		ChainClusterDB c1j = lookup1.get(chain1j);
+		ChainClusterDB c2i = lookup2.get(chain2i);
+		ChainClusterDB c2j = lookup2.get(chain2j);
+		
+		int sc1i = getSeqClusterId(c1i, SeqClusterLevel.C80);
+		int sc1j = getSeqClusterId(c1j, SeqClusterLevel.C80);
+		int sc2i = getSeqClusterId(c2i, SeqClusterLevel.C80);
+		int sc2j = getSeqClusterId(c2j, SeqClusterLevel.C80);
+		
+		if (sc1i==sc2i && sc1j==sc2j) {
+			return true;
+		}
+		if (sc1i==sc2j && sc1j==sc2i) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public static HashMap<String,ChainClusterDB> getChainIdLookup(PdbInfoDB pdb) {
+		HashMap<String,ChainClusterDB> lookup = new HashMap<String,ChainClusterDB>();
+		for (ChainClusterDB cc:pdb.getChainClusters()) {
+			lookup.put(cc.getRepChain(), cc);
+			if (cc.getMemberChains()!=null) {
+				for (String chain:cc.getMemberChains().split(",")) {
+					lookup.put(chain,cc);
+				}
+			}
+		}
+		return lookup;
+	}
+	
+	public static Pair<ChainClusterDB> getChainClusterPair(InterfaceDB interf) {
+		
+		ChainClusterDB cc1 = null;
+		ChainClusterDB cc2 = null;
+		
+		return new Pair<ChainClusterDB>(cc1,cc2);
 	}
 }
