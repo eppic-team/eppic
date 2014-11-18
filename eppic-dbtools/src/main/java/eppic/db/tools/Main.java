11fbde4edff020e1e309c14f640da3f76223fd08
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2014-11-18 14:57:18 +0100

    Nicer error message

--- eppic-dbtools/src/main/java/eppic/db/tools/Main.java
+++ eppic-dbtools/src/main/java/eppic/db/tools/Main.java
@@ -21,15 +21,20 @@ public class Main {
 			Class<?> toolClass = Class.forName("eppic.db.tools." + toolName);
 			Method main = toolClass.getMethod("main", String[].class);
 			main.invoke(null, new Object[] { toolParams });
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			System.err.println("Unexpected error: "+e.getMessage());
 			e.printStackTrace();
+			System.exit(1);
+		} catch (ClassNotFoundException e) {
+			System.err.println("The given tool '"+toolName+"' is not recognised as one of the supported tools");
 			printUsageAndExit();
 		}
 	}
 
 	private static void printUsageAndExit() {
 		System.err
-				.println("The first parameter must be the name of a tool.\n Available tools: ClusterCrystalForms, ClusterSequences, CompareLattices, UploadToDb, UserJobCopier");
+				.println("The first parameter must be the name of a tool.\n"
+						+ " Available tools: ClusterCrystalForms, ClusterSequences, CompareLattices, UploadToDb, UserJobCopier");
 		System.exit(1);
 	}
 
