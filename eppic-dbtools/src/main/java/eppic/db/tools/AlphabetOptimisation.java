84143739b9e284940441c8d28935b9a79b4faa0e
Joseph Christian Somody <somody@gmail.com>
2015-02-24 18:24:36 +0100

    Some more work on the alphabet optimisation

--- eppic-dbtools/src/main/java/eppic/db/tools/AlphabetOptimisation.java
+++ eppic-dbtools/src/main/java/eppic/db/tools/AlphabetOptimisation.java
@@ -14,6 +14,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -24,8 +25,7 @@ import owl.core.structure.AAAlphabet;
 
 public class AlphabetOptimisation {
 	
-	//public static final double CORE_RIM_CUTOFF = 0.80;
-	public static final double CORE_SURFACE_CUTOFF = 0.80;
+	public static final double EVOL_CUTOFF = 0.80;
 	public static final double GEOM_CUTOFF = 0.90;
 	public static final double MIN_ASA_FOR_SURFACE = 5;
 	public static final String[] AMINO_ACIDS = {"A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"};
@@ -33,8 +33,10 @@ public class AlphabetOptimisation {
 	public static final int INITIAL_POPULATION_SIZE = 10;
 	public static final int ROUNDS_OF_EVOLUTION = 50;
 	
-	public static ArrayList<ArrayList<MultipleSequenceAlignment>> MSAs;
-	public static ArrayList<ArrayList<Integer>> classes;
+	public static ArrayList<Boolean> truths; // THIS IS A LIST OF BOOLEANS (TRUE IFF BIO)
+	public static ArrayList<ArrayList<MultipleSequenceAlignment>> MSAs; // THIS IS A LIST OF PAIRS OF MSAS
+	public static ArrayList<ArrayList<ArrayList<Integer>>> regions; // THIS IS A LIST OF PAIRS OF LISTS OF REGIO_INTS
+	public static ArrayList<ArrayList<HashMap<Integer, Integer>>> mappings; // THIS IS A LIST OF PAIRS OF MAPPINGS FROM MSA_COL TO RES_ID
 
 	public static void main(String[] args) throws Exception {
 		String help = 
@@ -63,7 +65,7 @@ public class AlphabetOptimisation {
 			case '?':
 				System.err.println(help);
 				System.exit(1);
-				break; // getopt() already printed an error
+				break;
 			}
 		}
 		
@@ -78,19 +80,23 @@ public class AlphabetOptimisation {
 			System.exit(1);
 		}
 		
+		// START DATABASE HANDLER
 		DBHandler dbh = new DBHandler(dbName);
 		
+		// EXTRACT INFORMATION FROM DATASET
 		List<ArrayList<String>> listList = readList(listFile);
 		List<String> pdbCodes = listList.get(0);
 		List<String> firstChains = listList.get(1);
 		List<String> secondChains = listList.get(2);
 		List<String> interfaceNumbers = listList.get(3);
 		List<String> trueCalls = listList.get(4);
-		
-		MSAs = new ArrayList<ArrayList<MultipleSequenceAlignment>>();
-		classes = new ArrayList<ArrayList<Integer>>();
-
 		List<PdbInfoDB> pdbInfos = dbh.deserializePdbList(pdbCodes);
+		
+		// INITIALISE STATIC CLASS VARIABLES
+		truths = new ArrayList<Boolean>();
+		MSAs = new ArrayList<ArrayList<MultipleSequenceAlignment>>();
+		regions = new ArrayList<ArrayList<ArrayList<Integer>>>();
+		mappings = new ArrayList<ArrayList<HashMap<Integer, Integer>>>();
 		
 //		for (int i = 0; i < pdbInfos.size(); i++) {
 		for (int i = 0; i < 1; i++) {
@@ -99,48 +105,47 @@ public class AlphabetOptimisation {
 			String firstChain = firstChains.get(i);
 			String secondChain = secondChains.get(i);
 			int interfaceNumber = Integer.parseInt(interfaceNumbers.get(i));
-			boolean trueCall = trueCalls.get(i).equals("bio");
 			
+			// ADD TRUE CALL TO LIST OF TRUE CALLS
+			truths.add(trueCalls.get(i).equals("bio"));
+			
+			// GET LEFT AND RIGHT CHAINCLUSTERS FOR EACH PDBID
 			List<ChainClusterDB> chainClusters = pdbInfo.getChainClusters();
 			ChainClusterDB firstChainCluster = getChainClusterFromChainClustersByChain(chainClusters, firstChain);
 			ChainClusterDB secondChainCluster = getChainClusterFromChainClustersByChain(chainClusters, secondChain);
 			
-			List<InterfaceClusterDB> interfaceClusters = pdbInfo.getInterfaceClusters();
-			InterfaceDB theInterface = getInterfaceFromInterfaceClustersByInterfaceId(interfaceClusters, interfaceNumber);
-			List<ResidueDB> residues = theInterface.getResidues();
-			
-//			for (ResidueDB testRes : residues) {
-//				System.out.println(testRes.getPdbResidueNumber() + " " +
-//						testRes.getResidueNumber() + " " + testRes.getResidueType() + " " +
-//						testRes.getAsa() + " " + testRes.getBsa() + " " +
-//						getRegion(testRes.getAsa(), testRes.getBsa()) + " " + testRes.getSide());
-//			}
-			
-//			ArrayList<Integer> residueTypes = new ArrayList<Integer>();
-//			for (int j = 0; j < residues.size(); j++) {
-//				ResidueDB residue = residues.get(j);
-//				residueTypes.add(getRegion(residue.getAsa(), residue.getBsa()));
-//			}
-//			System.out.println("residues.size() = " + residues.size());
-//			allResidueTypes.add(residueTypes);
-			
+			// GET LEFT AND RIGHT HOMOLOGUES
 			List<HomologDB> firstHomologs = firstChainCluster.getHomologs();
 			List<HomologDB> secondHomologs = secondChainCluster.getHomologs();
 			
+			// CREATE ARRAYS FOR CREATING LEFT AND RIGHT MULTIPLESEQUENCEALIGNMENTS
 			String[] firstHomologTags = new String[firstHomologs.size() + 1];
 			String[] firstHomologSeqs = new String[firstHomologs.size() + 1];
 			String[] secondHomologTags = new String[secondHomologs.size() + 1];
 			String[] secondHomologSeqs = new String[secondHomologs.size() + 1];
 			
+			// CREATE TAGS AND SEQUENCES FOR LEFT AND RIGHT QUERIES AND ADD TO ARRAY
 			firstHomologTags[0] = firstChainCluster.getRefUniProtId() + "_" + firstChainCluster.getRefUniProtStart() + "-" + firstChainCluster.getRefUniProtEnd();
 			firstHomologSeqs[0] = firstChainCluster.getMsaAlignedSeq();
+			System.out.println("query-first-seq " + firstChainCluster.getMsaAlignedSeq());
 			secondHomologTags[0] = secondChainCluster.getRefUniProtId() + "_" + secondChainCluster.getRefUniProtStart() + "-" + secondChainCluster.getRefUniProtEnd();
 			secondHomologSeqs[0] = secondChainCluster.getMsaAlignedSeq();
+			//System.out.println("query-second-seq " + secondChainCluster.getMsaAlignedSeq());
 			
-//			System.out.println("first PDB = " + firstChainCluster.getPdbStart() + "-" + firstChainCluster.getPdbEnd());
-//			System.out.println("first REF = " + firstChainCluster.getRefUniProtStart() + "-" + firstChainCluster.getRefUniProtEnd());
-//			System.out.println("msa length = " + firstChainCluster.getMsaAlignedSeq().length());
+			// CREATE LEFT AND RIGHT MAPPINGS FROM MULTIPLESEQUENCEALIGNMENT COLUMN NUMBERS TO INTERFACE RESIDUE NUMBERS
+			HashMap<Integer, Integer> firstMapping = new HashMap<Integer, Integer>();
+			HashMap<Integer, Integer> secondMapping = new HashMap<Integer, Integer>();
+			// populate mappings here :P
 			
+			// CREATE PAIR (LEFT, RIGHT) OF MAPPINGS
+			ArrayList<HashMap<Integer, Integer>> mappingPair = new ArrayList<HashMap<Integer, Integer>>();
+			mappingPair.add(firstMapping);
+			mappingPair.add(secondMapping);
+			
+			// ADD PAIR OF LISTS OF REGIONS TO LIST OF PAIRS OF LISTS OF REGIONS
+			mappings.add(mappingPair);
+			
+			// ADD LEFT AND RIGHT HOMOLOGUE TAGS AND SEQUENCES TO ARRAY
 			for (int j = 0; j < firstHomologs.size(); j++) {
 				HomologDB homolog = firstHomologs.get(j);
 				firstHomologTags[j + 1] = homolog.getUniProtId() + "_" + homolog.getSubjectStart() + "-" + homolog.getSubjectEnd();
@@ -152,86 +157,138 @@ public class AlphabetOptimisation {
 				secondHomologSeqs[j + 1] = homolog.getAlignedSeq();
 			}
 			
+			// CREATE PAIR (LEFT, RIGHT) OF MULTIPLESEQUENCEALIGNMENTS
 			ArrayList<MultipleSequenceAlignment> MSAPair = new ArrayList<MultipleSequenceAlignment>();
 			MSAPair.add(new MultipleSequenceAlignment(firstHomologTags, firstHomologSeqs));
 			MSAPair.add(new MultipleSequenceAlignment(secondHomologTags, secondHomologSeqs));
 			
+			// ADD MULTIPLESEQUENCEALIGNMENT PAIR TO LIST OF MULTIPLESEQUENCEALIGNMENTS
 			MSAs.add(MSAPair);
 			
-			for (ArrayList<MultipleSequenceAlignment> aPair : MSAs) {
-				for (MultipleSequenceAlignment anMSA : aPair) {
-					anMSA.printSimple();
+			// GET LEFT AND RIGHT LISTS OF REGIONS OF RESIDUES
+			List<InterfaceClusterDB> interfaceClusters = pdbInfo.getInterfaceClusters();
+			InterfaceDB theInterface = getInterfaceFromInterfaceClustersByInterfaceId(interfaceClusters, interfaceNumber);
+			List<ResidueDB> residues = theInterface.getResidues();
+			ArrayList<Integer> firstRegions = new ArrayList<Integer>();
+			ArrayList<Integer> secondRegions = new ArrayList<Integer>();
+			for (ResidueDB aResidue : residues) {
+				if (aResidue.getSide() == 1) {
+					firstRegions.add(getRegion(aResidue.getAsa(), aResidue.getBsa()));					
+					System.out.println(aResidue.getResidueType() + aResidue.getResidueNumber() + " ");
+				}
+				else if (aResidue.getSide() == 2) {
+					secondRegions.add(getRegion(aResidue.getAsa(), aResidue.getBsa()));
 				}
 			}
-						
-//			int firstLowestPdb = Integer.MAX_VALUE;
-//			for (ResidueDB aResidue : residues) {
-//				if (aResidue.getSide() == 1 && Integer.parseInt(aResidue.getPdbResidueNumber()) < firstLowestPdb) {
-//					firstLowestPdb = Integer.parseInt(aResidue.getPdbResidueNumber());
-//				}
-//			}
-//			int firstHighestPdb = Integer.MIN_VALUE;
-//			for (ResidueDB aResidue : residues) {
-//				if (aResidue.getSide() == 1 && Integer.parseInt(aResidue.getPdbResidueNumber()) > firstLowestPdb) {
-//					firstHighestPdb = Integer.parseInt(aResidue.getPdbResidueNumber());
-//				}
-//			}
-//			for (int j = firstLowestPdb; j <= firstHighestPdb; j++) {
-//				int msaResSeqIndex = j - (firstChainCluster.getRefUniProtStart() - firstChainCluster.getPdbStart());
-//				char msaChar = MSAPair.get(0).getColumn(MSAPair.get(0).seq2al(MSAPair.get(0).getTagFromIndex(0), msaResSeqIndex)).charAt(0);
-//				ResidueDB jRes = null;
-//				for (ResidueDB aRes : residues) {
-//					if (Integer.parseInt(aRes.getPdbResidueNumber()) == j && aRes.getSide() == 1) {
-//						jRes = aRes;
-//					}
-//				}
-//				System.out.println(msaChar + "\t" + jRes.getResidueType());
-//			}
+			
+			// CREATE PAIR (LEFT, RIGHT) OF LISTS OF REGIONS OF RESIDUES
+			ArrayList<ArrayList<Integer>> regionPair = new ArrayList<ArrayList<Integer>>();
+			regionPair.add(firstRegions);
+			regionPair.add(secondRegions);
+			
+			// ADD PAIR OF LISTS OF REGIONS TO LIST OF PAIRS OF LISTS OF REGIONS
+			regions.add(regionPair);
 		}
 		
-		/*
-		 * Note to self for after back from vacation
-		 * 
-		 * alphabet evolution thing looks okay (that's what's below here)
-		 * for the mapping of the residue information (extracted from interfaces)
-		 * onto the MSA (extracted from homologues from chaincluster)
-		 * need to fix this...really weird now
-		 * PDB vs REF seqences in chaincluster can have gaps in the alignment!!
-		 * therefore, must first map the PDB to the REF properly (not JUST by difference in indices [shift])
-		 * and THEN once that's done, can use MSAalignedseq indices to map onto MSA itself
-		 * 
-		 * entire idea here: have two things extracted in parallel and then mapped
-		 * need to basically assign a label (region) to each column in MSA
-		 * then calculated core--surface score by looking at difference of entropies between labels
-		 * 
-		 * note also: don't don't don't use the String pdbResidueNumber from the ResidueDB
-		 * this can't be used for ANYTHING numeric
-		 * use the int residueNumber field from ResidueDB instead, it's the only one that's reliably sequential
-		 * that needs to be fixed in the above code...along with lots more 
-		 * 
-		 * okay also note that the region labels are a bit odd...anything "2" is also "3", since the geom
-		 * cutoff is higher (more stringent) than the evol one...so if you want to look at core vs surface
-		 * need to look at 0 vs 2+3
-		 */
-		
-//		for (int i = 0; i < 1; i++) {
-//			System.out.println(pdbCodes.get(i) + " " + interfaceNumbers.get(i) + " left");
-//			MSAs.get(i).get(0).printSimple();
-//			System.out.println(pdbCodes.get(i) + " " + interfaceNumbers.get(i) + " right");
-//			MSAs.get(i).get(1).printSimple();
-//		}
-		
-//		Set<AAAlphabet> alphabetPool = initialisePool();
-//		for (AAAlphabet alphabet : alphabetPool) {
-//			System.out.println(alphabet + "\t" + testFitness(alphabet));
-//		}
-//		for (int i = 1; i <= ROUNDS_OF_EVOLUTION; i++) {
-//			alphabetPool = evolve(alphabetPool);
-//			System.out.println("\nAfter " + i + " rounds of evolution...");
-//			for (AAAlphabet alphabet : alphabetPool) {
-//				System.out.println(alphabet + "\t" + testFitness(alphabet));
+		// RUN ALPHABET OPTIMISATION
+		System.out.println(truths);
+//		for (ArrayList<MultipleSequenceAlignment> msaPair : MSAs) {
+//			for (MultipleSequenceAlignment msa : msaPair) {
+//				msa.printSimple();
 //			}
 //		}
+		System.out.println(regions);
+		//optimiseAlphabet();
+	}
+	
+	private static ArrayList<ArrayList<String>> readList(File file) throws IOException {
+		ArrayList<String> pdbs = new ArrayList<String>();
+		ArrayList<String> ones = new ArrayList<String>();
+		ArrayList<String> twos = new ArrayList<String>();
+		ArrayList<String> ints = new ArrayList<String>();
+		ArrayList<String> trueCalls = new ArrayList<String>();
+		
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = br.readLine()) != null) {
+			if (line.startsWith("#")) continue;
+			if (line.trim().isEmpty()) continue;
+			String[] lineArray = line.trim().split("\\s+");
+			if (lineArray[1].length() == 4) {
+				 pdbs.add(lineArray[1]);
+				 ones.add(lineArray[2]);
+				 twos.add(lineArray[3]);
+				 ints.add(lineArray[4]);
+				 trueCalls.add(lineArray[5]);
+			}
+		}
+		br.close();
+		
+		ArrayList<ArrayList<String>> toReturn = new ArrayList<ArrayList<String>>();
+		toReturn.add(pdbs);
+		toReturn.add(ones);
+		toReturn.add(twos);
+		toReturn.add(ints);
+		toReturn.add(trueCalls);
+		return toReturn;
+	}
+	
+	private static ChainClusterDB getChainClusterFromChainClustersByChain(List<ChainClusterDB> clusters, String chain) {
+		ChainClusterDB toReturn = null;
+		for (ChainClusterDB cluster : clusters) {
+			for (String member : cluster.getMemberChains().split(",")) {
+				if (member.equals(chain)) {
+					toReturn = cluster;
+				}
+			}
+		}
+		return toReturn;
+	}
+	
+	private static InterfaceDB getInterfaceFromInterfaceClustersByInterfaceId(List<InterfaceClusterDB> clusters, int interfaceId) {
+		InterfaceDB toReturn = null;
+		for (InterfaceClusterDB cluster : clusters) {
+			for (InterfaceDB anInterface : cluster.getInterfaces()) {
+				if (anInterface.getInterfaceId() == interfaceId) {
+					toReturn = anInterface;
+				}
+			}
+		}
+		return toReturn;
+	}
+	
+	private static int getRegion(double asa, double bsa) {
+		int assignment = ResidueDB.OTHER;
+		double ratio = bsa / asa;
+		if (asa > MIN_ASA_FOR_SURFACE && bsa > 0) {
+			if (ratio < EVOL_CUTOFF) {
+				assignment = ResidueDB.RIM_EVOLUTIONARY;
+			}
+			else if (ratio < GEOM_CUTOFF) {
+				assignment = ResidueDB.CORE_EVOLUTIONARY; 
+			}
+			else {
+				assignment = ResidueDB.CORE_GEOMETRY;
+			}
+		}
+		else if (asa > MIN_ASA_FOR_SURFACE) {
+			assignment = ResidueDB.SURFACE;
+		}		
+		return assignment;
+	}
+	
+	private static void optimiseAlphabet() {
+		Set<AAAlphabet> alphabetPool = initialisePool();
+		for (AAAlphabet alphabet : alphabetPool) {
+			System.out.println(alphabet + "\t" + testFitness(alphabet));
+		}
+		for (int i = 1; i <= ROUNDS_OF_EVOLUTION; i++) {
+			alphabetPool = evolve(alphabetPool);
+			System.out.println("\nAfter " + i + " rounds of evolution...");
+			for (AAAlphabet alphabet : alphabetPool) {
+				System.out.println(alphabet + "\t" + testFitness(alphabet));
+			}
+		}
 	}
 	
 	private static Set<AAAlphabet> initialisePool() {
@@ -243,7 +300,18 @@ public class AlphabetOptimisation {
 	}
 	
 	private static int testFitness(AAAlphabet alphabet) {
-		// this will eventually be replaced by something that evaluates the scores for each pdbid
+//		this will eventually be replaced by something that evaluates the scores for each pdbid
+//		going to have to take the alphabet as a parameter
+//		will then use the alphabet to calculate entropies for the global static MSAs
+//		will then need to sample them while referring to regions and calculate score
+//		maybe that last part can be done using the code already in EPPIC
+		
+//		don't use the String pdbResidueNumber from the ResidueDB, it's not always an int...
+//		use the int residueNumber field from ResidueDB instead
+//		also, for regions, anything "2" is also "3", since the geom
+//		cutoff is higher (more stringent) than the evol one...
+//		so to look at core vs surface, need to look at 0 vs 2+3
+		
 		int fitness = 1;
 		if (alphabet.getGroupByOneLetterCode('D') == alphabet.getGroupByOneLetterCode('E')) fitness++;
 		if (alphabet.getGroupByOneLetterCode('H') == alphabet.getGroupByOneLetterCode('K')) fitness++;
@@ -301,81 +369,5 @@ public class AlphabetOptimisation {
 			}
 		}
 		return new AAAlphabet(newString.substring(0, newString.length() - 1));
-	}
-	
-	private static ChainClusterDB getChainClusterFromChainClustersByChain(List<ChainClusterDB> clusters, String chain) {
-		ChainClusterDB toReturn = null;
-		for (ChainClusterDB cluster : clusters) {
-			for (String member : cluster.getMemberChains().split(",")) {
-				if (member.equals(chain)) {
-					toReturn = cluster;
-				}
-			}
-		}
-		return toReturn;
-	}
-	
-	private static InterfaceDB getInterfaceFromInterfaceClustersByInterfaceId(List<InterfaceClusterDB> clusters, int interfaceId) {
-		InterfaceDB toReturn = null;
-		for (InterfaceClusterDB cluster : clusters) {
-			for (InterfaceDB anInterface : cluster.getInterfaces()) {
-				if (anInterface.getInterfaceId() == interfaceId) {
-					toReturn = anInterface;
-				}
-			}
-		}
-		return toReturn;
-	}
-	
-	private static int getRegion(double asa, double bsa) {
-		int assignment = ResidueDB.OTHER;
-		double ratio = bsa / asa;
-		if (asa > MIN_ASA_FOR_SURFACE && bsa > 0) {
-			if (ratio < CORE_SURFACE_CUTOFF) {
-				assignment = ResidueDB.RIM_EVOLUTIONARY;
-			}
-			else if (ratio < GEOM_CUTOFF) {
-				assignment = ResidueDB.CORE_EVOLUTIONARY; 
-			}
-			else {
-				assignment = ResidueDB.CORE_GEOMETRY;
-			}
-		}
-		else if (asa > MIN_ASA_FOR_SURFACE) {
-			assignment = ResidueDB.SURFACE;
-		}		
-		return assignment;
-	}
-	
-	private static ArrayList<ArrayList<String>> readList(File file) throws IOException {
-		ArrayList<String> pdbs = new ArrayList<String>();
-		ArrayList<String> ones = new ArrayList<String>();
-		ArrayList<String> twos = new ArrayList<String>();
-		ArrayList<String> ints = new ArrayList<String>();
-		ArrayList<String> trueCalls = new ArrayList<String>();
-		
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String line;
-		while ((line = br.readLine()) != null) {
-			if (line.startsWith("#")) continue;
-			if (line.trim().isEmpty()) continue;
-			String[] lineArray = line.trim().split("\\s+");
-			if (lineArray[1].length() == 4) {
-				 pdbs.add(lineArray[1]);
-				 ones.add(lineArray[2]);
-				 twos.add(lineArray[3]);
-				 ints.add(lineArray[4]);
-				 trueCalls.add(lineArray[5]);
-			}
-		}
-		br.close();
-		
-		ArrayList<ArrayList<String>> toReturn = new ArrayList<ArrayList<String>>();
-		toReturn.add(pdbs);
-		toReturn.add(ones);
-		toReturn.add(twos);
-		toReturn.add(ints);
-		toReturn.add(trueCalls);
-		return toReturn;
 	}
 }
