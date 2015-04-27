eppic-team/eppic

commit 3cbb77dd53a7ce93532846bcc29defb4da530a86

Now using Compound.getAlignedResIndex from biojava 4.1

eppic-cli/src/main/java/eppic/ChainEvolContext.java
@@ -24,7 +24,6 @@ import org.biojava.nbio.structure.Atom;
 import org.biojava.nbio.structure.Chain;
 import org.biojava.nbio.structure.Compound;
 import org.biojava.nbio.structure.Group;
-import org.biojava.nbio.structure.ResidueNumber;
 import org.biojava.nbio.structure.StructureTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -117,10 +116,9 @@ public class ChainEvolContext implements Serializable {
 	
 	private ChainEvolContextList parent;
 	
-	private HashMap<ResidueNumber,Integer> resNumbers2SeqresSerials;
-	private HashMap<Integer, ResidueNumber> seqresSerials2resNumbers;
-	
 	private boolean isProtein;
+	
+	private Compound compound;
 	
 
 	/**
@@ -137,6 +135,7 @@ public class ChainEvolContext implements Serializable {
 		this.searchWithFullUniprot = true;
 		this.queryWarnings = new ArrayList<String>();
 		this.isProtein = true;
+		this.compound = null;
 	}
 	
 	/**
@@ -175,8 +174,7 @@ public class ChainEvolContext implements Serializable {
 		
 		this.isProtein = StructureTools.isProtein(chain);
 		
-		// we only do this mapping in the case that the input is from chain
-		initResNumberMaps(compound);
+		this.compound = compound;
 	}
 	
 	public String getSequenceId() {
@@ -1105,46 +1103,7 @@ public class ChainEvolContext implements Serializable {
 	 * @return the SEQRES serial (1 to n) or -1 if not found
 	 */
 	public int getSeqresSerial(Group g) {
-		Integer resSerial = resNumbers2SeqresSerials.get(g.getResidueNumber());
-		if (resSerial==null) return -1;
-		return (int)resSerial;			
-	}
-	
-	/**
-	 * Returns the ResidueNumber given the SEQRES serial
-	 * @param resser
-	 * @return the ResidueNumber or null if not found (for instance for any residue that has no AtomGroup)
-	 */
-	public ResidueNumber getResNumFromSerial(int resser) {
-		return seqresSerials2resNumbers.get(resser);
-	}
-	
-	/**
-	 * Return the SEQRES serial corresponding to the given ResidueNumber 
-	 * @param resNum
-	 * @return the SEQRES serial (1 to n) or -1 if not found
-	 */
-	public int getSerialFromResNum(ResidueNumber resNum) {
-		Integer resSerial = resNumbers2SeqresSerials.get(resNum);
-		if (resSerial==null) return -1;
-		return (int)resSerial;			
-	}
-	
-	private void initResNumberMaps(Compound compound) {
-		resNumbers2SeqresSerials = new HashMap<ResidueNumber, Integer>();
-		seqresSerials2resNumbers = new HashMap<Integer, ResidueNumber>();
-
-		// TODO revise it this would work for raw PDB files without an actual SEQRES: we choose the seqresGroups from each chain but I'm not sure it that would be valid in raw PDB file case
-		// we have to go through every Chain in the Compound to be sure that every observed residue in the whole Compound
-		// is included in the map
-		for (Chain c:compound.getChains()) {
-
-			List<Group> seqresGroups = c.getSeqResGroups();
-			for (int i=0;i<seqresGroups.size();i++) {			
-				resNumbers2SeqresSerials.put(seqresGroups.get(i).getResidueNumber(),i+1);
-				seqresSerials2resNumbers.put(i+1, seqresGroups.get(i).getResidueNumber());
-			}
-		}
+		return compound.getAlignedResIndex(g, compound.getRepresentative());
 	}
 	
 	/**

eppic-cli/src/main/java/eppic/DataModelAdaptor.java
@@ -68,6 +68,8 @@ public class DataModelAdaptor {
 	
 	private static final double NO_BURIAL_AVAILABLE = -1.0;
 	
+	private static final int UNKNOWN_RESIDUE_INDEX = -1;
+	
 	public static final String PDB_BIOUNIT_METHOD = "pdb1";
 	
 	private PdbInfoDB pdbInfo;
@@ -209,8 +211,14 @@ public class DataModelAdaptor {
 					ContactDB contact = new ContactDB();
 					Group firstGroup = groupContact.getPair().getFirst();
 					Group secondGroup = groupContact.getPair().getSecond();
-					contact.setFirstResNumber(getSeqresSerial(firstGroup, firstGroup.getChain()) );
-					contact.setSecondResNumber(getSeqresSerial(secondGroup, secondGroup.getChain()) );
+					if (firstGroup.getChain().getCompound()==null)
+						contact.setFirstResNumber(UNKNOWN_RESIDUE_INDEX);
+					else 
+						contact.setFirstResNumber(firstGroup.getChain().getCompound().getAlignedResIndex(firstGroup, firstGroup.getChain()) );
+					if (secondGroup.getChain().getCompound()==null)
+						contact.setSecondResNumber(UNKNOWN_RESIDUE_INDEX);
+					else
+						contact.setSecondResNumber(secondGroup.getChain().getCompound().getAlignedResIndex(secondGroup, secondGroup.getChain()) );
 					contact.setFirstResType(firstGroup.getPDBName());
 					contact.setSecondResType(secondGroup.getPDBName());
 					GroupAsa firstGroupAsa = interf.getFirstGroupAsa(firstGroup.getResidueNumber());
@@ -781,7 +789,10 @@ public class DataModelAdaptor {
 
 
 			ResidueDB iri = new ResidueDB();
-			iri.setResidueNumber(getSeqresSerial(group, chain));
+			if (chain.getCompound()==null)
+				iri.setResidueNumber(UNKNOWN_RESIDUE_INDEX);
+			else 
+				iri.setResidueNumber(chain.getCompound().getAlignedResIndex(group, chain));
 			iri.setPdbResidueNumber(group.getResidueNumber().toString());
 			iri.setResidueType(resType);
 			iri.setAsa(asa);
@@ -917,24 +928,6 @@ public class DataModelAdaptor {
 			i++;
 		}
 		return sb.toString();
-	}
-	
-	/**
-	 * Returns the SEQRES serial of the Group g in the Chain c
-	 * @param g
-	 * @param c
-	 * @return the SEQRES serial (1 to n) or -1 if not found
-	 */
-	public static int getSeqresSerial(Group g, Chain c) {
-		
-		// this is not very efficient, it would be nice to store the mapping in a cached map somewhere 
-		List<Group> seqresGroups = c.getSeqResGroups();
-		for (int i=0;i<seqresGroups.size();i++) {
-			if (seqresGroups.get(i) == g) 
-				return i+1;
-		}
-		// not found, return -1
-		return -1;
 	}
 	
 	public static boolean isDisulfideInteraction(AtomContact contact) {

eppic-cli/src/main/java/eppic/PymolRunner.java
@@ -640,9 +640,9 @@ public class PymolRunner {
 			// we need to escape the negative residues in pymol with a backslash
 			if (pdbSerial.startsWith("-")) pdbSerial = "\\"+pdbSerial;
 			
-			int currentSerial = DataModelAdaptor.getSeqresSerial(list.get(i), c);
+			int currentSerial = c.getCompound().getAlignedResIndex(list.get(i), c);
 			int prevSerial = -1;
-			if (i>0) prevSerial = DataModelAdaptor.getSeqresSerial(list.get(i-1),c);
+			if (i>0) prevSerial = c.getCompound().getAlignedResIndex(list.get(i-1),c);
 			
 			if (i==0) {
 				lastSerial =  currentSerial;

commit 9c1d01cd063fa61f5fe7e8733c1fe9ef8396c6ad

Fixed bug introduced in 3cbb77dd53a7ce93532846bcc29defb4da530a86

eppic-cli/src/main/java/eppic/ChainEvolContext.java
@@ -1096,7 +1096,7 @@ public class ChainEvolContext implements Serializable {
 	 * @return the SEQRES serial (1 to n) or -1 if not found
 	 */
 	public int getSeqresSerial(Group g) {
-		return compound.getAlignedResIndex(g, compound.getRepresentative());
+		return compound.getAlignedResIndex(g, g.getChain());
 	}
 	
 	/**