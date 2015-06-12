e934a0725fa7b7d51797debdbddeae251dbd49cb
josemduarte <jose.m.duarte@gmail.com>
2015-06-12 15:12:57 +0200

    Fixed bug in assembly clustering: was throwing away valid assemblies

--- eppic-cli/src/main/java/eppic/assembly/AssemblyGroup.java
+++ eppic-cli/src/main/java/eppic/assembly/AssemblyGroup.java
@@ -5,12 +5,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AssemblyGroup implements Iterable<Assembly> {
 
-	//private static final Logger logger = LoggerFactory.getLogger(AssemblyGroup.class);
+	private static final Logger logger = LoggerFactory.getLogger(AssemblyGroup.class);
 			
 			
 	private List<Assembly> list;
@@ -52,23 +52,40 @@ public class AssemblyGroup implements Iterable<Assembly> {
 	
 	public List<AssemblyGroup> sortIntoClusters() {
 		
+		logger.info("Clustering assembly group: {}",toString());
+		
 		List<Assembly> maximals = getLargestAssemblies();
 		
+		// here we store the clusters that we'll return
+		List<AssemblyGroup> clusters = new ArrayList<AssemblyGroup>();
 		
-		List<AssemblyGroup> sortedGroups = new ArrayList<AssemblyGroup>();
+		// here we keep track of which assemblies in this group have been clustered already
+		List<Assembly> inClusters = new ArrayList<Assembly>(this.size());
 		
 		for (Assembly max: maximals) {
 			AssemblyGroup g = new AssemblyGroup();
 			g.add(max); // the first member will always be the maximal group, so we can choose it as the representative of the cluster
-			sortedGroups.add(g);
+			inClusters.add(max);
+			clusters.add(g);
 			for (Assembly a:list) {
 				if (a==max) continue;
 				if (max.isChild(a)) {
 					g.add(a);
+					inClusters.add(a);
 				}
 			}
 		}
-		return sortedGroups;
+		
+		AssemblyGroup notInClusters = new AssemblyGroup();
+		for (Assembly a:this) {
+			if (!inClusters.contains(a)) notInClusters.add(a);			
+		}
+		if (notInClusters.size()>0) {
+			logger.info("Unclustered assemblies left: {}. Going for another round of clustering",notInClusters.toString());
+			clusters.addAll(notInClusters.sortIntoClusters());
+		}
+		
+		return clusters;
 	}
 	
 	public int size() {
