eppic-team/eppic

commit da80a191e87549684515bfba33ea4d14abdc2064

Now setting the ATOM_CACHE_PATH for the lattice graph page in wui.
Like that we can reuse the atom cache dir in production both for eppic
cli runs and for the wui and we avoid unnecessary redownloading of
cif.gz files.

eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/server/commons/servlets/BaseServlet.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.systemsx.sybit.crkwebui.server.CrkWebServiceImpl;
+import eppic.EppicParams;
 
 /**
  * Base class for other servlets.
@@ -28,6 +29,7 @@ public class BaseServlet extends HttpServlet {
 	private static final Logger logger = LoggerFactory.getLogger(BaseServlet.class);
 
 	protected Properties properties;
+	protected Properties propertiesCli;
 
 	public void init(ServletConfig config) throws ServletException 
 	{
@@ -41,12 +43,24 @@ public class BaseServlet extends HttpServlet {
 			InputStream propertiesStream = 
 					new FileInputStream(new File(CrkWebServiceImpl.SERVER_PROPERTIES_FILE));
 
-			properties.load(propertiesStream);
+			properties.load(propertiesStream);			
+			
 		}
 		catch (IOException e) 
 		{
-			//e.printStackTrace();
+			
 			throw new ServletException("Properties file '"+CrkWebServiceImpl.SERVER_PROPERTIES_FILE+"' can not be read. Error: "+e.getMessage());
+		}
+		
+		File eppicCliConfFile = new File(System.getProperty("user.home"), EppicParams.CONFIG_FILE_NAME);
+		
+		try {
+			logger.info("Reading CLI properties file {} ", eppicCliConfFile.toString());
+			
+			propertiesCli.load(new FileInputStream(eppicCliConfFile));
+
+		} catch (IOException e) {
+			throw new ServletException("Properties file '"+eppicCliConfFile+"' can not be read. Error: "+e.getMessage());
 		}
 	}
 

eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/server/jmol/generators/LatticeGraphPageGenerator.java
@@ -16,9 +16,11 @@ import org.biojava.nbio.structure.PDBCrystallographicInfo;
 import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.StructureException;
 import org.biojava.nbio.structure.StructureIO;
+import org.biojava.nbio.structure.align.util.AtomCache;
 import org.biojava.nbio.structure.contact.AtomContactSet;
 import org.biojava.nbio.structure.contact.StructureInterface;
 import org.biojava.nbio.structure.contact.StructureInterfaceCluster;
+import org.biojava.nbio.structure.io.LocalPDBDirectory.FetchBehavior;
 import org.biojava.nbio.structure.io.PDBFileParser;
 import org.biojava.nbio.structure.io.mmcif.MMcifParser;
 import org.biojava.nbio.structure.io.mmcif.SimpleMMcifConsumer;
@@ -42,8 +44,9 @@ public class LatticeGraphPageGenerator {
 	 * Generates html page containing the 3Dmol canvas.
 	 * 
 	 * @param directory path to the job directory
-	 * @param structFilename Path to the input structure
-	 * @param ucFilename Path to the unit cell structure. Will be generated if doesn't exist
+	 * @param inputName the input: either a PDB id or the file name as input by user
+	 * @param atomCachePath the path for Biojava's AtomCache
+	 * @param ucFile Path to the unit cell structure. Will be generated if doesn't exist
 	 * @param ucURI URL to reach ucFilename within the browser
 	 * @param title Page title [default: structure name]
 	 * @param size the canvas size 
@@ -55,7 +58,7 @@ public class LatticeGraphPageGenerator {
 	 * @throws StructureException For errors parsing the input structure
 	 * @throws IOException For errors reading or writing files
 	 */
-	public static void generatePage(File directory, String inputName, File ucFile,
+	public static void generatePage(File directory, String inputName, String atomCachePath, File ucFile,
 			String ucURI, String title, String size, List<Interface> interfaces,
 			Collection<Integer> requestedIfaces, String url3dmoljs, PrintWriter out) throws IOException, StructureException {
 
@@ -84,7 +87,23 @@ public class LatticeGraphPageGenerator {
 				InputStream inStream = new FileInputStream(structFile);
 				auStruct = parser.parsePDBFile(inStream);
 			}
+		} else if (!inputName.matches("^\\d\\w\\w\\w$")) {
+				logger.error("Could not find file {} and the inputName '{}' does not look like a PDB id. Can't produce the lattice graph page!", structFile.toString(), inputName);
+				return;
 		} else {
+			// it is like a PDB id, leave it to AtomCache
+			AtomCache atomCache = null;
+			if (atomCachePath ==null) 
+				atomCache = new AtomCache();
+			else 
+				atomCache = new AtomCache(atomCachePath);
+		
+			// we set it to FETCH_FILES to avoid going to the PDB ftp server, because we trust in principle what we have in our cache dir (which is rsynced externally or by the eppic cli run)
+			atomCache.setFetchBehavior(FetchBehavior.FETCH_FILES);
+			
+			StructureIO.setAtomCache(atomCache);
+		
+			
 			// leave it to atomcache
 			auStruct = StructureIO.getStructure(inputName);
 		}

eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/server/jmol/servlets/LatticeGraphServlet.java
@@ -67,6 +67,8 @@ public class LatticeGraphServlet extends BaseServlet
 
 	private String resultsLocation;
 	private String destination_path;
+	
+	private String atomCachePath;
 
 	@Override
 	public void init(ServletConfig config) throws ServletException
@@ -75,6 +77,10 @@ public class LatticeGraphServlet extends BaseServlet
 
 		resultsLocation = properties.getProperty("results_location");
 		destination_path = properties.getProperty("destination_path");
+		atomCachePath = propertiesCli.getProperty("ATOM_CACHE_PATH");
+		
+		if (atomCachePath == null) 
+			logger.warn("ATOM_CACHE_PATH is not set in config file, will not be able to reuse cache for PDB cif.gz files!");
 	}
 
 	@Override
@@ -128,7 +134,7 @@ public class LatticeGraphServlet extends BaseServlet
 
 			outputStream = new PrintWriter(response.getOutputStream());
 
-			LatticeGraphPageGenerator.generatePage(dir,input, ucFile, ucURI, title, size, ifaceList, requestedIfaces, url3dmoljs,outputStream);
+			LatticeGraphPageGenerator.generatePage(dir,input, atomCachePath, ucFile, ucURI, title, size, ifaceList, requestedIfaces, url3dmoljs,outputStream);
 
 		}
 		catch(ValidationException e)

commit be305b64a077ada206d147f648f8710789948c3e

Fixing bug introduced in da80a191e87549684515bfba33ea4d14abdc2064

eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/server/commons/servlets/BaseServlet.java
@@ -36,6 +36,7 @@ public class BaseServlet extends HttpServlet {
 		super.init(config);
 
 		properties = new Properties();
+		propertiesCli = new Properties();
 
 		try {
 			