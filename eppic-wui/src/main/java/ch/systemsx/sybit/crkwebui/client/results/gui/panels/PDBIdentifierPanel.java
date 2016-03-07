9bd663f95d30c4621c6599cc478fcddf8b714641
parkera <althea.parker@id.ethz.ch>
2016-03-07 16:07:44 +0100

    Issue #92 Angsroms & cleanup from issue #93

--- eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/client/results/gui/panels/PDBIdentifierPanel.java
+++ eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/client/results/gui/panels/PDBIdentifierPanel.java
@@ -23,11 +23,8 @@ public class PDBIdentifierPanel extends HorizontalLayoutContainer
 {
 	private FlexTable panelTable;
 	
-	//public static EppicLabel informationLabel;
-	//public static EppicLabel pdbNameLabel;
 	public static HTML informationLabel;
 	public static HTML pdbNameLabel;
-	//private LabelWithTooltip warningLabel;
 	public static LabelWithTooltip warningLabel;
 	
 	public PDBIdentifierPanel(int viewType)
@@ -38,13 +35,6 @@ public class PDBIdentifierPanel extends HorizontalLayoutContainer
 		
 		this.add(panelTable, new HorizontalLayoutData(-1,-1));
 		
-		/*if(viewType == ResultsPanel.ASSEMBLIES_VIEW){
-			informationLabel = new EppicLabel(EscapedStringGenerator.generateEscapedString(
-						AppPropertiesManager.CONSTANTS.info_panel_pdb_identifier() + ": "));
-		}else if(viewType == ResultsPanel.INTERFACES_VIEW){
-			informationLabel = new EppicLabel(EscapedStringGenerator.generateEscapedString(
-						AppPropertiesManager.CONSTANTS.info_panel_interface_pdb_identifier() + ": "));
-		}*/
 		if(viewType == ResultsPanel.ASSEMBLIES_VIEW){
 			informationLabel = new HTML(EscapedStringGenerator.generateEscapedString(
 						AppPropertiesManager.CONSTANTS.info_panel_pdb_identifier() + ": "));
@@ -56,17 +46,7 @@ public class PDBIdentifierPanel extends HorizontalLayoutContainer
 		informationLabel.addStyleName("eppic-pdb-identifier-label");
 		
 	}
-	
-	/*public void switchTitle(int viewType){
-		if(viewType ==ResultsPanel.ASSEMBLIES_VIEW)
-			informationLabel = new EppicLabel(
-				EscapedStringGenerator.generateEscapedString(
-						AppPropertiesManager.CONSTANTS.info_panel_pdb_identifier() + ": "));
-		else if(viewType ==ResultsPanel.INTERFACES_VIEW)
-		informationLabel = new EppicLabel(
-				EscapedStringGenerator.generateEscapedString(
-						AppPropertiesManager.CONSTANTS.info_panel_interface_pdb_identifier() + ": "));
-	}*/
+
 	
 	/**
 	 * Sets values of pdb identifier.
@@ -84,56 +64,16 @@ public class PDBIdentifierPanel extends HorizontalLayoutContainer
 			  			   int inputType)
 	{
 		
-		//panelTable.clear();
-		//Window.alert("setting PDBText with " + ApplicationContext.getPdbInfo().getPdbCode());
-		//could really set this here with the navigation text, that's the only place we can locate it here
-		//pdbNameLabel = new HTML(ApplicationContext.getPdbInfo().getPdbCode() + "!");
-		
-		
-		//this gets set first, always!! and then the stuff in MainController gets called. 
-		//pdbNameLabel = new HTML(inputName);
-		//pdbNameLabel = new HTML("xxx");
-		
-		
 		pdbNameLabel = new HTML(inputName);
-		
-		/*if(inputType == InputType.PDBCODE.getIndex())
-		{
-			pdbNameLabel = new LinkWithTooltip(EscapedStringGenerator.generateEscapedString(inputName),
-								AppPropertiesManager.CONSTANTS.pdb_identifier_panel_label_hint(),
-								 ApplicationContext.getSettings().getPdbLinkUrl() + inputName);
-		}
-		else
-		{
-			pdbNameLabel = new EppicLabel(EscapedStringGenerator.generateEscapedString(inputName));
-			
-		}*/
-		//String code = ApplicationContext.getPdbInfo().getPdbCode();
-		//pdbNameLabel = new EppicLabel(code);
 		
 		//Check for warnings
 		ExperimentalWarnings warnings = new ExperimentalWarnings(spaceGroup, expMethod, resolution, rfreeValue);
 		warningLabel = warnings.getWarningLabel();
-		//moved this somewhere else - now consolidated inside the ExperimentalWarnings object
-		/*if (warnings.isEmWarning()) {
-			warningLabel = createWarningLabel(AppPropertiesManager.CONSTANTS.warning_EM_title());			
-		}
-		else if(warnings.isResolutionWarning()) {			
-			warningLabel = createWarningLabel(AppPropertiesManager.CONSTANTS.warning_LowRes_title());
-		}else if(warnings.isRfreeWarning()){
-			warningLabel = createWarningLabel(AppPropertiesManager.CONSTANTS.warning_HighRfree_title());
-		}else{
-			warningLabel = null;
-		}*/
 		
 		pdbNameLabel.addStyleName("eppic-pdb-identifier-label");
 		panelTable.setWidget(0, 0, informationLabel);
 		panelTable.setWidget(0, 1, pdbNameLabel);
 		
-		//if(warningLabel != null)
-		//	panelTable.setWidget(0, 2, warningLabel);
-		
-		//test only: force a warning
 		if(warningLabel != null)
 			panelTable.setWidget(0, 2, warningLabel);
 		else
@@ -147,16 +87,6 @@ public class PDBIdentifierPanel extends HorizontalLayoutContainer
 	{
 		pdbNameLabel = new HTML(inputName);
 	}	
-	
-	
-	
-	//moved this
-	/*private LabelWithTooltip createWarningLabel(String text){
-		LabelWithTooltip label = new LabelWithTooltip("*"+text+"*", AppPropertiesManager.CONSTANTS.pdb_identifier_panel_warning_hint());
-		label.addStyleName("eppic-header-warning");
-		label.addStyleName("eppic-pdb-identifier-label");
-		return label;
-	}*/
 	
 	private LabelWithTooltip createEmptyWarningLabel(String text){
 		LabelWithTooltip label = new LabelWithTooltip("", AppPropertiesManager.CONSTANTS.pdb_identifier_panel_warning_hint());
