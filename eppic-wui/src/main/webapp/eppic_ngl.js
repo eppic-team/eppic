386c4d32653f5546223d93af83adcd32b7f925dc
josemduarte <jose.m.duarte@gmail.com>
2016-03-13 23:00:07 -0700

    Adding entropy potato feature

--- eppic-wui/src/main/webapp/eppic_ngl.js
+++ eppic-wui/src/main/webapp/eppic_ngl.js
@@ -1,5 +1,24 @@
 var stage;
+var structureComponent;
+var surf;
+var surfaceOn = false;
+var currentChainSurfIndex = 0;
 
+
+document.onkeypress = function (e) {
+    e = e || window.event;
+
+    if (e !== undefined && e.which == 112) { // 112 is p
+    	console.log("p was pressed, toggling surface");
+    	toggleSurface();
+    }
+    
+    if (e !== undefined && e.which == 110) { // 110 is n
+    	//console.log("n was pressed, drawing next surface");
+    	nextSurface();
+    }
+
+};
 
 function onInit() {
 	stage = new NGL.Stage("viewport");
@@ -16,14 +35,14 @@ function initRepr(structComp) {
 
 	structureComponent = structComp;
 
-	ligandRepr = structComp.addRepresentation('ball+stick', {
+	ligandRepr = structureComponent.addRepresentation('ball+stick', {
 		color : 'element',
 		scale : 3.0,
 		aspectRatio : 1.3,
 		sele : 'hetero and not (water or ion)'
 	});
 
-	waterRepr = structComp.addRepresentation('licorice',{
+	waterRepr = structureComponent.addRepresentation('licorice',{
 		color : 'element',
 		sele  : 'water'
 	});
@@ -69,4 +88,41 @@ function initRepr(structComp) {
 
 	stage.centerView();
 
+}
+
+function toggleSurface() {
+
+	if (surf === undefined) {
+		showSurface(chains[currentChainSurfIndex])
+		surfaceOn = true;
+	} else {
+		if (surfaceOn) {
+			surf.setVisibility(false);
+			surfaceOn = false;
+		} else {
+			surf.setVisibility(true);
+			surfaceOn = true;
+		}
+
+
+	}
+}
+
+function nextSurface() {
+	if (surfaceOn) {
+		currentChainSurfIndex++;
+		if (currentChainSurfIndex==chains.length) currentChainSurfIndex = 0;
+		showSurface(chains[currentChainSurfIndex]);
+	}
+}
+
+function showSurface(chain) {
+	surf = structureComponent.addRepresentation('surface', {
+		colorScheme: 'bfactor',
+		//opacity: 0.7,
+		colorDomain: [2.58,0],
+		colorScale: 'roygb',
+		//surfaceType: 'sas',
+		sele: ':'+chain
+	});
 }
