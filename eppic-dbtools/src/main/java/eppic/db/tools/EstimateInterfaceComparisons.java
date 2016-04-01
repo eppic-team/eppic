e0ff133df98f68d930e6e84769bd126b72b4bc7c
Spencer Bliven <sbliven@ucsd.edu>
2016-04-01 11:35:55 +0200

    EstimateInterfaceComparisons enhancements
    - Add -o option to output all clusters
    - Fix bug with non-protein interfaces
    - Fix overflow in total interface calculation

--- eppic-dbtools/src/main/java/eppic/db/tools/EstimateInterfaceComparisons.java
+++ eppic-dbtools/src/main/java/eppic/db/tools/EstimateInterfaceComparisons.java
@@ -1,10 +1,14 @@
 package eppic.db.tools;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.biojava.nbio.structure.contact.Pair;
 import org.slf4j.Logger;
@@ -29,13 +33,15 @@ public class EstimateInterfaceComparisons {
 				"Usage: FindBiomimics \n" +		
 				"  -D         : the database name to use\n"+
 				"  -l         : sequence cluster level to be used (default "+DEFAULT_SEQ_CLUSTER_LEVEL.getLevel()+"\n"+
+				"  -o         : Output file, in tsv format giving the sequence cluster IDs and number of interfaces.\n"+
 				"The database access parameters must be set in file "+DBHandler.CONFIG_FILE_NAME+" in home dir\n";
 		
 		
 		String dbName = null;
 		SeqClusterLevel seqClusterLevel = DEFAULT_SEQ_CLUSTER_LEVEL;
+		String outputFilename = null;
 		
-		Getopt g = new Getopt("FindBiomimics", args, "D:p:c:l:o:h?");
+		Getopt g = new Getopt("FindBiomimics", args, "D:l:o:h?");
 		int c;
 		while ((c = g.getopt()) != -1) {
 			switch(c){
@@ -44,6 +50,9 @@ public class EstimateInterfaceComparisons {
 				break;
 			case 'l':
 				seqClusterLevel = SeqClusterLevel.getByLevel(Integer.parseInt(g.getOptarg())); 
+				break;
+			case 'o':
+				outputFilename = g.getOptarg();
 				break;
 			case 'h':
 				System.out.println(help);
@@ -89,6 +98,11 @@ public class EstimateInterfaceComparisons {
 					int seqA = chainA.getSeqClusterId(seqClusterLevel);
 					int seqB = chainB.getSeqClusterId(seqClusterLevel);
 					
+					// skip chains that didn't cluster, e.g. DNA
+					if(seqA < 0 || seqB < 0) {
+						continue;
+					}
+					
 					// create sorted pair
 					Pair<Integer> seqPair;
 					if( seqA <= seqB) {
@@ -116,15 +130,33 @@ public class EstimateInterfaceComparisons {
 		}
 		updateProgress(pdbNum,allPDBs.size());
 
+
 		// Output
-		System.out.format("Found %d non-redundant interfaces in cluster %d%%%n",interfaceCounts.size(),seqClusterLevel.getLevel());
-		outputHistogram(interfaceCounts.values(),30);
-		
-		long comparisons = 0;
-		for(int x: interfaceCounts.values()) {
-			comparisons += x*(x-1)/2;
+		System.out.format("Found %d non-redundant interfaces in cluster %d%%%n",
+				interfaceCounts.size(),seqClusterLevel.getLevel());
+
+		if(outputFilename != null) {
+			try( PrintWriter out = new PrintWriter(outputFilename) ) {
+				out.println("SeqCluster1\tSeqCluster2\tMembers");
+				for(Entry<Pair<Integer>, Integer> entry : interfaceCounts.entrySet()) {
+					int a = entry.getKey().getFirst();
+					int b = entry.getKey().getSecond();
+					int count = entry.getValue();
+					out.format("%d\t%d\t%d%n", a,b,count);
+				}
+			} catch( IOException e) {
+				logger.error("Error writing {}",outputFilename,e);
+			}
 		}
-		System.out.format("Requires %d comparisons%n",comparisons);
+
+
+		outputHistogram(interfaceCounts.values(),60);
+
+		BigInteger comparisons = BigInteger.valueOf(0);
+		for(int x: interfaceCounts.values()) {
+			comparisons = comparisons.add(BigInteger.valueOf(x*(x-1)/2));
+		}
+		System.out.format("Requires %d (%.3g) comparisons%n",comparisons,comparisons.doubleValue());
 	}
 
 	private static void outputHistogram(Collection<Integer> values, int bins) {
@@ -157,9 +189,10 @@ public class EstimateInterfaceComparisons {
 		double var = ( totalSq - ((double)total*total)/n )/(n-1);
 		
 		// two bins for outliers, then (bins-2) for the central +-2*sigma
-		int binwidth = Math.max(1, Math.round((float)(Math.sqrt(var)*4./(bins-2))));
-		int firstBin = (int)(mean-binwidth*(bins/2.-1));
-		
+		//int binwidth = Math.max(1, Math.round((float)(Math.sqrt(var)*4./(bins-2))));
+		//int firstBin = (int)(mean-binwidth*(bins/2.-1));
+		int binwidth=1;
+		int firstBin=2;
 		int[] binvals = new int[bins];
 		for( int x : values) {
 			if(x<firstBin) {
@@ -188,7 +221,9 @@ public class EstimateInterfaceComparisons {
 		for(int i=1;i<bins-1;i++) {
 			printBin(firstBin+binwidth*i, firstBin+binwidth*(i+1)-1, binvals[i], countsPerChar);
 		}
-		printBin(firstBin+binwidth*(bins-1),Math.max(max,firstBin+binwidth*bins-1),binvals[bins-1],countsPerChar); 
+		printBin(firstBin+binwidth*(bins-1),Math.max(max,firstBin+binwidth*bins-1),binvals[bins-1],countsPerChar);
+		System.out.format("Mean=%f%n",mean);
+		System.out.format("StdDev=%f%n",Math.sqrt(var));
 	}
 	private static void printBin(int min, int max, int counts, double countsPerChar) {
 		System.out.format("%4d - %4d:%5d [",min,max,counts);
