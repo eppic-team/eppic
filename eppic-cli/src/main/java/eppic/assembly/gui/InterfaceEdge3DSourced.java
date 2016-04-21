b86c16aa03bd9a29defb92a3230673ea2e9dc178
Spencer Bliven <sbliven@ucsd.edu>
2016-04-21 16:14:07 +0200

    Fix compilation problems

--- eppic-cli/src/main/java/eppic/assembly/gui/InterfaceEdge3DSourced.java
+++ eppic-cli/src/main/java/eppic/assembly/gui/InterfaceEdge3DSourced.java
@@ -40,8 +40,8 @@ public class InterfaceEdge3DSourced<V> extends InterfaceEdge3D {
 	 * @param graph
 	 * @return graph, with InterfaceEdge3D replaced by InterfaceEdge3DSourced
 	 */
+	@SuppressWarnings("unchecked")
 	public static <V> Pseudograph<V,InterfaceEdge3DSourced<V>> addSources( Graph<V,InterfaceEdge3D> graph ){
-		@SuppressWarnings("unchecked")
 		Pseudograph<V,InterfaceEdge3DSourced<V>> out = new Pseudograph<V, InterfaceEdge3DSourced<V>>((Class<? extends InterfaceEdge3DSourced<V>>) InterfaceEdge3DSourced.class);
 		for(V vert : graph.vertexSet() ) {
 			out.addVertex(vert);
