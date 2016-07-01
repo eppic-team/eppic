b6133cf50af954329fb43af58b1e774c4577fb7f
parkera <althea.parker@id.ethz.ch>
2016-07-01 12:52:15 +0200

    Non enantiomorphic space group warning

--- eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/shared/helpers/ExperimentalWarnings.java
+++ eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/shared/helpers/ExperimentalWarnings.java
@@ -1,6 +1,7 @@
 package ch.systemsx.sybit.crkwebui.shared.helpers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.gwt.user.client.Window;
@@ -20,10 +21,19 @@ public class ExperimentalWarnings {
 	private boolean resolutionWarning;
 	private boolean rfreeWarning;
 	private boolean noRfreeWarning;
+	private boolean spaceGroupWarning;
 	private String warningTooltip;
 	//public static LabelWithTooltip staticWarningsLabel = null;
 	
-
+	public String[] spaceGroups = {"P 1", "P 2", "P 21", "P 1 2 1", "P 1 21 1", "C 2", "C 1 2 1", "P 2 2 2", "P 2 2 21", "P 21 21 2",
+									"P 21 21 21", "C 2 2 2", "C 2 2 21", "F 2 2 2", "I 2 2 2", "I 21 21 21", "P 4", "P 41", "P 42",
+									"P 43", "I 4", "I 41", "P 4 2 2", "P 4 21 2", "P 41 2 2", "P 41 21 2", "P 42 2 2", "P 42 21 2",
+									"P 43 2 2", "P 43 21 2", "I 4 2 2", "I 4 21 2", "P 3", "P 31", "P 32", "R 3", "P 3 1 2",
+									"P 31 1 2", "P 32 1 2", "P 3 2 1", "P 31 2 1", "P 32 2 1", "R 3 2", "P 6", "P 61", "P 62",
+									"P 63", "P 64", "P 65", "P 6 2 2", "P 61 2 2", "P 62 2 2", "P 63 2 2", "P 64 2 2", "P 65 2 2",
+									"P 2 3", "P 21 3", "F 2 3", "I 2 3", "I 21 3", "P 4 3 2", "P 42 3 2", "P 43 3 2", "P 41 3 2",
+									"F 4 3 2", "F 41 3 2", "I 4 3 2", "I 41 3 2"};
+	
 	public ExperimentalWarnings(String spaceGroup,
 	   String expMethod,
 	   double resolution,
@@ -36,6 +46,7 @@ public class ExperimentalWarnings {
 		rfreeWarning = (ApplicationContext.getSettings().getRfreeCutOff() > 0 && 
 				        rFree > ApplicationContext.getSettings().getRfreeCutOff() && rFree > 0 && rFree<1);
 		noRfreeWarning = (rFree == 1 && expMethod.equals("X-RAY DIFFRACTION"));
+		spaceGroupWarning = (expMethod.equals("X-RAY DIFFRACTION") && !Arrays.asList(spaceGroups).contains(spaceGroup));
 	}
 
 	public boolean isEmWarning() {
@@ -64,6 +75,14 @@ public class ExperimentalWarnings {
 
 	public void setRfreeWarning(boolean rfreeWarning) {
 		this.rfreeWarning = rfreeWarning;
+	}
+	
+	public boolean isSpaceGroupWarning(){
+		return spaceGroupWarning;
+	}
+	
+	public void setSpaceGroupWarning(boolean spaceGroupWarning){
+		this.spaceGroupWarning = spaceGroupWarning;
 	}
 	
 	//for single warnings only
@@ -115,6 +134,12 @@ public class ExperimentalWarnings {
 			warningCount++;
 			multiWarningTooltip += "&bull; " + AppPropertiesManager.CONSTANTS.warning_NoRfree_title().replace("Warning: ", "") + "<br>" + AppPropertiesManager.CONSTANTS.warning_NoRfree_text() + "<br>";
 		}
+		if(this.isSpaceGroupWarning()){
+			warningTooltip = AppPropertiesManager.CONSTANTS.warning_SpaceGroup_text();
+			warningLabel = createWarningLabel(AppPropertiesManager.CONSTANTS.warning_SpaceGroup_title(), warningTooltip);	
+			warningCount++;
+			multiWarningTooltip += "&bull; " + AppPropertiesManager.CONSTANTS.warning_SpaceGroup_title().replace("Warning: ", "") + "<br>" + AppPropertiesManager.CONSTANTS.warning_SpaceGroup_text() + "<br>";
+		}		
 		if(warningCount > 1){
 			warningLabel = createWarningLabel(warningCount + " Warnings", multiWarningTooltip);
 		}
