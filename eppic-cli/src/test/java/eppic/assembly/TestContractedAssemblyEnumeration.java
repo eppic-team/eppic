e8afbcd31e423214224db5a4ab8e228d493f6ff3
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2016-10-13 17:41:51 -0700

    Adding another test

--- eppic-cli/src/test/java/eppic/assembly/TestContractedAssemblyEnumeration.java
+++ eppic-cli/src/test/java/eppic/assembly/TestContractedAssemblyEnumeration.java
@@ -3,6 +3,7 @@ package eppic.assembly;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.biojava.nbio.structure.Structure;
 import org.biojava.nbio.structure.StructureException;
@@ -15,27 +16,28 @@ public class TestContractedAssemblyEnumeration {
 	public void test4nwp() throws IOException, StructureException {
 		// 2 entities and a few assemblies, largest assembly is tetrahedral
 		
-//		Structure s = TestLatticeGraph.getStructure("4nwp");
-//		
-//		StructureInterfaceList interfaces = TestLatticeGraph.getAllInterfaces(s);
-//		
-//		CrystalAssemblies crystalAssemblies = new CrystalAssemblies(s, interfaces, false);
-//		
-//		CrystalAssemblies crystalAssembliesC = new CrystalAssemblies(s, interfaces, true);
-//		
-//		
-//		System.out.printf("%d assemblies found using full graph: \n", crystalAssemblies.size());
-//		
-//		for (Assembly a : crystalAssemblies) {
-//			System.out.println("assembly "+a.toString());
-//		}
-//
-//		System.out.printf("%d assemblies found using contracted graph: \n", crystalAssembliesC.size());
-//		
-//		for (Assembly a : crystalAssembliesC) {
-//			System.out.println("assembly "+a.toString());
-//			
-//		}
+		Structure s = TestLatticeGraph.getStructure("4nwp");
+		
+		StructureInterfaceList interfaces = TestLatticeGraph.getAllInterfaces(s);
+		
+		//CrystalAssemblies crystalAssemblies = new CrystalAssemblies(s, interfaces, false);
+		
+		CrystalAssemblies crystalAssembliesC = new CrystalAssemblies(s, interfaces, true);
+		
+		
+		//System.out.printf("%d assemblies found using full graph: \n", crystalAssemblies.size());
+		
+		//for (Assembly a : crystalAssemblies) {
+		//	System.out.println("assembly "+a.toString() + " -- " + getDescription(a));
+		//}
+
+		System.out.printf("%d assemblies found using contracted graph: \n", crystalAssembliesC.size());
+		
+		for (Assembly a : crystalAssembliesC.getUniqueAssemblies()) {
+			
+			System.out.println("assembly "+a.toString() + " -- " + getDescription(a));
+			
+		}
 		
 		
 	}
@@ -79,6 +81,19 @@ public class TestContractedAssemblyEnumeration {
 		
 
 		
+	}
+	
+	private static String getDescription(Assembly a) {
+		
+		StringBuilder sb = new StringBuilder();
+		int i = -1;
+		for (List<SubAssembly> sas : a.getAssemblyGraph().getSubAssembliesGroupedByStoichiometries()) {
+			SubAssembly sa = sas.get(0);
+			i++;
+			sb.append(sa.getStoichiometry()+"/"+sa.getSymmetry());
+			if (i!=a.getAssemblyGraph().getSubAssemblies().size()-1) sb.append(",");
+		}
+		return sb.toString();
 	}
 
 }
