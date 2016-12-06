43dd0009ec86232ed2be03d22144c7234b63bdc5
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2016-12-05 16:59:10 -0800

    An attempt to have a very basic display of pdb biounit, #100

--- eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/client/results/data/AssemblyItemModelProperties.java
+++ eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/client/results/data/AssemblyItemModelProperties.java
@@ -1,9 +1,5 @@
 package ch.systemsx.sybit.crkwebui.client.results.data;
 
-import java.util.List;
-
-import ch.systemsx.sybit.crkwebui.shared.model.InterfaceWarning;
-
 import com.google.gwt.editor.client.Editor.Path;
 import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
