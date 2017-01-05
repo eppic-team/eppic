2887166b4052503c05463a8266f96c541de8c256
lafita <aleixlafita@gmail.com>
2017-01-05 12:45:06 +0100

    Unique way to calculate confidence of calls for cluster predictors

--- eppic-cli/src/main/java/eppic/predictors/CombinedClusterPredictor.java
+++ eppic-cli/src/main/java/eppic/predictors/CombinedClusterPredictor.java
@@ -87,10 +87,10 @@ public class CombinedClusterPredictor implements InterfaceTypePredictor {
 		
 		switch (call) {
 		case BIO: 
-			confidence = (probability - 0.5) / 0.5;
+			confidence = probability;
 			break;
 		case CRYSTAL: 
-			confidence = (0.5 - probability) / 0.5;
+			confidence = 1 - probability;
 			break;
 		case NO_PREDICTION: 
 			confidence = CONFIDENCE_UNASSIGNED;
