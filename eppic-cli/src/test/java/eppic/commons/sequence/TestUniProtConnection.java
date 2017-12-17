b53030ac78094d41a1df543dd6146c76decf537b
Jose Duarte <jose.m.duarte@gmail.com>
2017-12-17 11:25:30 -0800

    Adding test for uniprot api connection

---
+++ eppic-cli/src/test/java/eppic/commons/sequence/TestUniProtConnection.java
@@ -0,0 +1,19 @@
+package eppic.commons.sequence;
+
+import org.junit.Test;
+
+import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
+import uk.ac.ebi.uniprot.dataservice.client.exception.ServiceException;
+
+import static org.junit.Assert.*;
+
+public class TestUniProtConnection {
+
+	@Test
+	public void testGetUniProt() throws NoMatchFoundException, ServiceException {
+		String upid = "P30340";
+		UniProtConnection upc = new UniProtConnection();
+		UniProtEntry entry = upc.getEntry(upid);
+		assertEquals(upid, entry.getUniProtId().toString());
+	}
+}
