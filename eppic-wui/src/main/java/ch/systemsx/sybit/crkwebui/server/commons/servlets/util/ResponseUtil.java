88714e10cdcbd98a9fba6398e7423890c54f0ca8
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2018-08-21 17:56:37 -0700

    Error handling

--- eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/server/commons/servlets/util/ResponseUtil.java
+++ eppic-wui/src/main/java/ch/systemsx/sybit/crkwebui/server/commons/servlets/util/ResponseUtil.java
@@ -22,45 +22,16 @@ public class ResponseUtil {
 										  		HttpServletResponse response) throws IOException
 	{
 		byte[] buffer = new byte[1024];
-		DataInputStream input = null;
-		ServletOutputStream output = null;
-		
-		try
-		{
-			input = new DataInputStream(new FileInputStream(resultFile));
-			output = response.getOutputStream();
-			
+
+		try (
+				DataInputStream input = new DataInputStream(new FileInputStream(resultFile));
+				ServletOutputStream output = response.getOutputStream()) {
+
 			int length;
 
 			while ((input != null)
-					&& ((length = input.read(buffer)) != -1)) 
-			{
+					&& ((length = input.read(buffer)) != -1)) {
 				output.write(buffer, 0, length);
-			}
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-			throw e;
-		}
-		finally
-		{
-			if(input != null)
-			{
-				try
-				{
-					input.close();
-				}
-				catch(Exception ex) {}
-			}
-			
-			if(output != null)
-			{
-				try
-				{
-					output.close();
-				}
-				catch(Exception ex) {}
 			}
 		}
 	}
