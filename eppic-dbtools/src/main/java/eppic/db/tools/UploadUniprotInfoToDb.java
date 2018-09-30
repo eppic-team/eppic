54518529b1384baf6f119c6d1a3a94a13bf0175d
Jose Duarte <jose.m.duarte@gmail.com>
2018-09-29 18:25:04 -0700

    Finished implementation of UploadUniprotInfoToDb

--- eppic-dbtools/src/main/java/eppic/db/tools/UploadUniprotInfoToDb.java
+++ eppic-dbtools/src/main/java/eppic/db/tools/UploadUniprotInfoToDb.java
@@ -3,9 +3,11 @@ package eppic.db.tools;
 import eppic.commons.sequence.NoMatchFoundException;
 import eppic.commons.sequence.UniProtConnection;
 import eppic.commons.sequence.UnirefEntry;
+import eppic.db.EntityManagerHandler;
 import eppic.db.dao.DaoException;
 import eppic.db.dao.UniProtInfoDAO;
 import eppic.db.dao.jpa.UniProtInfoDAOJpa;
+import eppic.db.jpautils.DbConfigGenerator;
 import gnu.getopt.Getopt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +17,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,12 +28,14 @@ public class UploadUniprotInfoToDb {
 
     private static final Logger logger = LoggerFactory.getLogger(UploadUniprotInfoToDb.class);
 
-    private static final Pattern UNIREF_SUBJECT_PATTERN = Pattern.compile("^UniRef\\d+_([0-9A-Z\\-]++)$");
+    private static final Pattern UNIREF_SUBJECT_PATTERN = Pattern.compile("^UniRef\\d+_([0-9A-Z\\-]+)$");
+
+    private static AtomicInteger failureCounter = new AtomicInteger(0);
 
     public static void main(String[] args) throws IOException {
 
         String help =
-                "Usage: UploadSearchSeqCacheToDb\n" +
+                "Usage: UploadUniprotInfoToDb\n" +
                         "  -D <string>  : the database name to use\n"+
                         "  -f <file>    : the blast tabular format file containing all hits\n" +
                         " [-g <file>]   : a configuration file containing the database access parameters, if not provided\n" +
@@ -79,36 +85,59 @@ public class UploadUniprotInfoToDb {
             System.exit(1);
         }
 
-        UniProtConnection uc = new UniProtConnection();
+        initJpaConnection(configFile);
 
         Set<String> uniqueIds = getUniqueUniRefIds(blastTabFile);
+
+        logger.info("A total of {} unique UniProt/Parc ids were found", uniqueIds.size());
+
+        UniProtConnection uc = new UniProtConnection();
 
         // possibly more efficient but needs to be done via batches in loop below. We can't request many millions
         // of them in one go because the output of UniRefEntry would be very large and require much memory
         //List<UnirefEntry> unirefs = uc.getMultipleUnirefEntries(uniprotIds);
 
+        ExecutorService executorService = Executors.newFixedThreadPool(numWorkers);
+
+        UniProtInfoDAO dao = new UniProtInfoDAOJpa();
+
         for (String uniId : uniqueIds) {
-            try {
-                // TODO what to do if it's a isoform id? (i.e. with a "-")
-
-                // TODO check if this works for uniparc and uniprot ids
-                UnirefEntry entry = uc.getUnirefEntry(uniId);
-
-                UniProtInfoDAO dao = new UniProtInfoDAOJpa();
-
-                dao.insertUniProtInfo(entry.getUniId(), entry.getSequence(), entry.getFirstTaxon(), entry.getLastTaxon());
-
-
-            } catch (NoMatchFoundException e) {
-                logger.warn("Could not find UniProt id {} via JAPI", uniId);
-            } catch (ServiceException e) {
-                logger.warn("Problem retrieving UniProt info from JAPI for UniProt id {}", uniId);
-            } catch (DaoException e) {
-                logger.warn("Could not insert to db UniProt id {}. Will not continue.", uniId);
-                break;
-            }
+            executorService.submit(() -> retrieveInfoAndPersist(uc, dao, uniId));
         }
 
+        executorService.shutdown();
+
+        while (!executorService.isTerminated()) {
+
+        }
+
+        logger.info("Done retrieving info and persisting {} UniProt/Parc entries to database", uniqueIds.size());
+        if (failureCounter.get()>0) {
+            logger.info("{} entries could not be retrieved or persisted", failureCounter.get());
+        }
+
+    }
+
+    private static void retrieveInfoAndPersist(UniProtConnection uc, UniProtInfoDAO dao, String uniId) {
+
+        // TODO what to do if it's a isoform id? (i.e. with a "-")
+
+        try {
+            // TODO check if this works for uniparc and uniprot ids
+            final UnirefEntry entry = uc.getUnirefEntry(uniId);
+
+            dao.insertUniProtInfo(entry.getUniId(), entry.getSequence(), entry.getFirstTaxon(), entry.getLastTaxon());
+
+        } catch (DaoException e) {
+            logger.warn("Could not insert to db UniProt id {}.", uniId);
+            failureCounter.incrementAndGet();
+        } catch (NoMatchFoundException e) {
+            logger.warn("Could not find UniProt id {} via JAPI", uniId);
+            failureCounter.incrementAndGet();
+        } catch (ServiceException e) {
+            logger.warn("Problem retrieving UniProt info from JAPI for UniProt id {}", uniId);
+            failureCounter.incrementAndGet();
+        }
     }
 
     private static Set<String> getUniqueUniRefIds(File blastTabFile) throws IOException {
@@ -131,7 +160,6 @@ public class UploadUniprotInfoToDb {
 
                 String subjectId = tokens[1];
 
-
                 Matcher m = UNIREF_SUBJECT_PATTERN.matcher(subjectId);
                 if (m.matches()) {
                     String uniId = m.group(1);
@@ -140,5 +168,10 @@ public class UploadUniprotInfoToDb {
             }
         }
         return uniqueIds;
+    }
+
+    private static void initJpaConnection(File configFile) throws IOException {
+        Map<String,String> props = DbConfigGenerator.createDatabaseProperties(configFile);
+        EntityManagerHandler.initFactory(props);
     }
 }
