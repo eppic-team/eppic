23368ff3ca86c162b8a30eb75c1ed02432e5b8c8
Jose Duarte <jose.m.duarte@gmail.com>
2018-10-20 16:44:00 -0700

    Implementation of UploadUniprotInfoToDb that doesn't use UniProt JAPI but UniRef FASTA file only

--- eppic-dbtools/src/main/java/eppic/db/tools/UploadUniprotInfoToDb.java
+++ eppic-dbtools/src/main/java/eppic/db/tools/UploadUniprotInfoToDb.java
@@ -1,8 +1,5 @@
 package eppic.db.tools;
 
-import eppic.commons.sequence.NoMatchFoundException;
-import eppic.commons.sequence.UniProtConnection;
-import eppic.commons.sequence.UnirefEntry;
 import eppic.db.EntityManagerHandler;
 import eppic.db.dao.DaoException;
 import eppic.db.dao.UniProtInfoDAO;
@@ -12,50 +9,52 @@ import eppic.model.dto.UniProtInfo;
 import gnu.getopt.Getopt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.ac.ebi.kraken.interfaces.uniparc.UniParcEntry;
-import uk.ac.ebi.uniprot.dataservice.client.exception.ServiceException;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
 public class UploadUniprotInfoToDb {
 
     private static final Logger logger = LoggerFactory.getLogger(UploadUniprotInfoToDb.class);
 
     private static final Pattern UNIREF_SUBJECT_PATTERN = Pattern.compile("^UniRef\\d+_([0-9A-Z\\-]+)$");
+    private static final Pattern TAXONOMY_PATTERN = Pattern.compile(".*Tax=(.*)TaxID=.*");
 
     private static AtomicInteger countDone = new AtomicInteger(0);
 
     private static AtomicInteger alreadyPresent = new AtomicInteger(0);
     private static AtomicInteger couldntInsert = new AtomicInteger(0);
-    private static AtomicInteger couldntRetrieve = new AtomicInteger(0);
     private static AtomicInteger couldntFind = new AtomicInteger(0);
+    private static AtomicInteger didInsert = new AtomicInteger(0);
 
     public static void main(String[] args) throws IOException {
 
         String help =
-                "Usage: UploadUniprotInfoToDb\n" +
+                        "Usage: UploadUniprotInfoToDb\n" +
                         "  -D <string>  : the database name to use\n"+
                         "  -f <file>    : the blast tabular format file containing all hits\n" +
+                        "  -a <file>    : UniRef FASTA file (plain text or gzipped, if gzipped must have .gz suffix)\n" +
                         " [-g <file>]   : a configuration file containing the database access parameters, if not provided\n" +
                         "                 the config will be read from file "+DBHandler.DEFAULT_CONFIG_FILE_NAME+" in home dir\n" +
-                        " [-n <int>]    : number of workers. Default 1. \n";
+                        " [-n <int>]    : number of workers. Default 1. \n" +
+                        "Reads all unique UniRef ids present in the .m8 file (output of blast/mmseqs), \n" +
+                        "then retrieves the sequence and taxonomy data (only last taxon) from the UniRef FASTA file and\n" +
+                        "finally persists the data to db\n";
 
 
         File blastTabFile = null;
+        File unirefFastaFile = null;
         String dbName = null;
         File configFile = DBHandler.DEFAULT_CONFIG_FILE;
         int numWorkers = 1;
 
-        Getopt g = new Getopt("UploadSearchSeqCacheToDb", args, "D:f:g:n:h?");
+        Getopt g = new Getopt("UploadSearchSeqCacheToDb", args, "D:f:a:g:n:h?");
         int c;
         while ((c = g.getopt()) != -1) {
             switch(c){
@@ -64,6 +63,9 @@ public class UploadUniprotInfoToDb {
                     break;
                 case 'f':
                     blastTabFile = new File(g.getOptarg());
+                    break;
+                case 'a':
+                    unirefFastaFile = new File(g.getOptarg());
                     break;
                 case 'g':
                     configFile = new File(g.getOptarg());
@@ -92,44 +94,25 @@ public class UploadUniprotInfoToDb {
             System.exit(1);
         }
 
-        initJpaConnection(configFile);
-
         logger.info("Parsing file {}", blastTabFile);
 
         Set<String> uniqueIds = getUniqueUniRefIds(blastTabFile);
 
         logger.info("A total of {} unique UniProt/Parc ids were found", uniqueIds.size());
 
-        UniProtConnection uc = new UniProtConnection();
+        initJpaConnection(configFile);
 
-        // possibly more efficient but needs to be done via batches in loop below. We can't request many millions
-        // of them in one go because the output of UniRefEntry would be very large and require much memory
-        //List<UnirefEntry> unirefs = uc.getMultipleUnirefEntries(uniprotIds);
+        retrieveInfoAndPersist(unirefFastaFile, uniqueIds, numWorkers);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(numWorkers);
-
-        UniProtInfoDAO dao = new UniProtInfoDAOJpa();
-
-        for (String uniId : uniqueIds) {
-            executorService.submit(() -> retrieveInfoAndPersist(uc, dao, uniId));
+        if (countDone.get()<uniqueIds.size()) {
+            logger.warn("Count of processed entries ({}) is less than the number of entries needed ({}). FASTA file did not contain some entries. There's something wrong!");
         }
-
-        executorService.shutdown();
-
-        while (!executorService.isTerminated()) {
-
-        }
-
-        int didInsert = uniqueIds.size() - couldntInsert.get() - couldntFind.get() - couldntRetrieve.get() - alreadyPresent.get();
-        logger.info("Done processing {} UniProt/Parc entries to database. Actually inserted {} in db", uniqueIds.size(), didInsert);
+        logger.info("Done processing {} UniProt/Parc entries to database. Actually inserted {} in db", uniqueIds.size(), didInsert.get());
         if (couldntInsert.get()>0) {
             logger.info("{} entries could not be persisted to db", couldntInsert.get());
         }
         if (couldntFind.get()>0) {
-            logger.info("{} entries could not be found by querying JAPI", couldntFind.get());
-        }
-        if (couldntRetrieve.get()>0) {
-            logger.info("{} entries failed to be retrieved from JAPI", couldntRetrieve.get());
+            logger.info("{} entries could not be found in UniRef FASTA file", couldntFind.get());
         }
         if (alreadyPresent.get()>0) {
             logger.info("{} entries were already present in db and were not reloaded", alreadyPresent.get());
@@ -137,22 +120,110 @@ public class UploadUniprotInfoToDb {
 
     }
 
-    private static void retrieveInfoAndPersist(UniProtConnection uc, UniProtInfoDAO dao, String uniId) {
+    /**
+     * Parses the FASTA file extracting id, taxonomy and sequence for ids present in input uniqueIds,
+     * subsequently persisting the info to db via JPA.
+     * @param fastaFile the UniRef FASTA file, either plain text or gzipped (must have .gz extension)
+     * @param uniqueIds the ids for which we want to extract info from the FASTA file
+     * @param numWorkers the number of workers for parallelising the persistence
+     * @throws IOException if problems when parsing FASTA file
+     */
+    private static void retrieveInfoAndPersist(File fastaFile, Set<String> uniqueIds, int numWorkers) throws IOException {
+
+        logger.info("Proceeding to parse info from FASTA file {} and persist it.", fastaFile);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numWorkers);
+
+        UniProtInfoDAO dao = new UniProtInfoDAOJpa();
+
+        InputStreamReader isr;
+        if (fastaFile.getName().endsWith(".gz")) {
+            isr = new InputStreamReader(new GZIPInputStream(new FileInputStream(fastaFile)));
+        } else {
+            isr = new InputStreamReader(new FileInputStream(fastaFile));
+        }
+        try (BufferedReader br = new BufferedReader(isr)) {
+            String line;
+            boolean readSequence = false;
+            UniEntry currentUniEntry = null;
+            StringBuilder currentSequence = null;
+            while ((line = br.readLine())!=null) {
+                if (line.trim().isEmpty()) continue;
+
+                if (line.startsWith(">")) {
+                    if (currentUniEntry!=null)
+                        currentUniEntry.sequence = currentSequence.toString();
+
+                    final UniEntry uniEntry = currentUniEntry;
+                    executorService.submit(() -> persist(dao, uniEntry));
+
+                    // reset last sequence
+                    currentSequence = new StringBuilder();
+                    readSequence = false;
+                    currentUniEntry = new UniEntry();
+
+                    // tag line, parse id to see if we want to persist it
+                    // >UniRef90_P20848 Putative alpha-1-antitrypsin-related protein n=1 Tax=Homo sapiens TaxID=9606 RepID=A1ATR_HUMAN
+                    String tag = line.substring(1, line.indexOf(" ")).trim();
+                    Matcher m = UNIREF_SUBJECT_PATTERN.matcher(tag);
+                    if (m.matches()) {
+                        String uniId = m.group(1);
+                        if (uniqueIds.contains(uniId)) {
+                            currentUniEntry.uniId = uniId;
+                            readSequence = true;
+                            Matcher taxMatcher = TAXONOMY_PATTERN.matcher(line);
+                            if (taxMatcher.matches()) {
+                                currentUniEntry.lastTaxon = taxMatcher.group(1).split(" ")[0];
+                            } else {
+                                logger.warn("Could not match taxonomy for uni id {}, won't have taxonomy info for it. Full tag is: {}", uniId, line);
+                            }
+                        }
+                    } else {
+                        logger.warn("Tag {} does not conform to expected UniRef90 identifier format", tag);
+                    }
+                } else {
+                    // line with sequence data
+                    if (readSequence) {
+                        currentSequence.append(line.trim());
+                    }
+                }
+            }
+
+            // make sure we persist the last sequence if it is one of the required ids
+            if (readSequence) {
+                currentUniEntry.sequence = currentSequence.toString();
+
+                final UniEntry uniEntry = currentUniEntry;
+                executorService.submit(() -> persist(dao, uniEntry));
+            }
+
+        } finally {
+            executorService.shutdown();
+
+            while (!executorService.isTerminated()) {
+
+            }
+
+        }
+
+    }
+
+    private static void persist(UniProtInfoDAO dao, UniEntry uniEntry) {
 
         // TODO what to do if it's a isoform id? (i.e. with a "-")
 
+        int countProcessed = countDone.incrementAndGet();
+
+        if (countProcessed%1000000 == 0) {
+            logger.info("Done processing {} entries", countProcessed);
+        }
+
         try {
 
-            int countProcessed = countDone.incrementAndGet();
-
-            if (countProcessed%1000000 == 0) {
-                logger.info("Done processing {} entries", countProcessed);
-            }
-
-            UniProtInfo uniProtInfo = dao.getUniProtInfo(uniId);
+            UniProtInfo uniProtInfo = dao.getUniProtInfo(uniEntry.uniId);
 
             if (uniProtInfo!=null) {
-                logger.debug("Id already present in db: {}. Skipping", uniId);
+                logger.debug("Id already present in db: {}. Skipping", uniEntry.uniId);
                 int countPresent = alreadyPresent.incrementAndGet();
                 if (countPresent % 1000000 == 0) {
                     logger.info("Gone through {} already present entries in db", countPresent);
@@ -160,31 +231,20 @@ public class UploadUniprotInfoToDb {
                 return;
             }
 
-            if (uniId.startsWith("UPI")) {
-                UniParcEntry uniparcEntry = uc.getUniparcEntry(uniId);
-                String seq = uniparcEntry.getSequence().getValue();
-                dao.insertUniProtInfo(uniId, seq, null, null);
-            } else {
-                final UnirefEntry entry = uc.getUnirefEntry(uniId);
-                dao.insertUniProtInfo(entry.getUniId(), entry.getSequence(), entry.getFirstTaxon(), entry.getLastTaxon());
-            }
+            dao.insertUniProtInfo(uniEntry.uniId, uniEntry.sequence, null, uniEntry.lastTaxon);
+            didInsert.incrementAndGet();
+
 
         } catch (DaoException e) {
-            logger.warn("Could not insert to db UniProt id {}.", uniId);
+            logger.warn("Could not insert to db UniProt id {}.", uniEntry.uniId);
             couldntInsert.incrementAndGet();
-        } catch (NoMatchFoundException e) {
-            logger.warn("Could not find UniProt id {} via JAPI", uniId);
-            couldntFind.incrementAndGet();
-        } catch (ServiceException e) {
-            logger.warn("Problem retrieving UniProt info from JAPI for UniProt id {}", uniId);
-            couldntRetrieve.incrementAndGet();
         }
     }
 
     private static Set<String> getUniqueUniRefIds(File blastTabFile) throws IOException {
         Set<String> uniqueIds = new HashSet<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(blastTabFile));) {
+        try (BufferedReader br = new BufferedReader(new FileReader(blastTabFile))) {
 
             String line;
             int lineNum = 0;
@@ -226,5 +286,11 @@ public class UploadUniprotInfoToDb {
     private static void initJpaConnection(File configFile) throws IOException {
         Map<String,String> props = DbConfigGenerator.createDatabaseProperties(configFile);
         EntityManagerHandler.initFactory(props);
+    }
+
+    private static class UniEntry {
+        String uniId;
+        String lastTaxon;
+        String sequence;
     }
 }
