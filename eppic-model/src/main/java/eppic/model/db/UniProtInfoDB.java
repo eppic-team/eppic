36ae39ff8bf4f53f9dc91e9301b1a0ea2f5970d1
Jose Manuel Duarte <jose.m.duarte@gmail.com>
2019-08-20 10:03:30 -0700

    Some more conversions to annotations

--- eppic-model/src/main/java/eppic/model/db/UniProtInfoDB.java
+++ eppic-model/src/main/java/eppic/model/db/UniProtInfoDB.java
@@ -1,14 +1,29 @@
 package eppic.model.db;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@Table(name="UniProtInfo",
+        indexes = @Index(name = "uniId_idx", columnList = "uniId", unique = true)) // All requests will be via uniId, so this is important
 public class UniProtInfoDB implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int uid;
 
     private String uniId;
+
+    // As of Sept 2018, info from uniprot.org: the longest sequence is A0A1V4K6M4 at 36,991 AA
+    @Column(length = 40000)
     private String sequence;
 
     private String firstTaxon;
