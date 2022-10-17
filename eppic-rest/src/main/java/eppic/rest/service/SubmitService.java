d3ecffa208f8cb9eadb20d6ec6e8e31c509d0f51
Jose Duarte <jose.m.duarte@gmail.com>
2022-10-17 16:51:21 -0700

    Docs

--- eppic-rest/src/main/java/eppic/rest/service/SubmitService.java
+++ eppic-rest/src/main/java/eppic/rest/service/SubmitService.java
@@ -13,6 +13,13 @@ public class SubmitService {
      */
     public Response submit(FileFormat fileFormat, boolean gzipped, String fileName, InputStream fileInputStream, String email) {
         // TODO implement
+        // Steps
+        // 1 validate
+        // 2 generate id
+        // 3 write to disk so that CLI can read
+        // 4 submit CLI job
+        // 5 return generated id
+        // Question: when to persist in db?
         return null;
 
     }
