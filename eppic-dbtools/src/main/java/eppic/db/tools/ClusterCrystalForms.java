5cf9184bcff0a82dfa881f3603e775b6fce951c5
Jose Duarte <jose.m.duarte@gmail.com>
2024-10-05 11:20:03 -0700

    Remove PdbInfo.uid

--- eppic-dbtools/src/main/java/eppic/db/tools/ClusterCrystalForms.java
+++ eppic-dbtools/src/main/java/eppic/db/tools/ClusterCrystalForms.java
@@ -260,7 +260,7 @@ public class ClusterCrystalForms {
 				
 				System.out.print(member.getPdbInfo().getPdbCode()+" ");
 				
-				if (cfcPw !=null ) cfcPw.println( member.getPdbInfo().getUid()+"\t"+
+				if (cfcPw !=null ) cfcPw.println( member.getPdbInfo().getEntryId()+"\t"+
 												member.getPdbInfo().getPdbCode()+"\t"+
 												seqClusterId+"\t"+
 												cfCluster.getId()+"\t"+
