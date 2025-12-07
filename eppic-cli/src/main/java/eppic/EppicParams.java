92d81ba864b8d75714b12d19a111fb546f65aee7
Jose Duarte <jose.m.duarte@gmail.com>
2025-12-06 22:24:56 -0800

    Imports

--- eppic-cli/src/main/java/eppic/EppicParams.java
+++ eppic-cli/src/main/java/eppic/EppicParams.java
@@ -4,7 +4,6 @@ import eppic.commons.sequence.AAAlphabet;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
